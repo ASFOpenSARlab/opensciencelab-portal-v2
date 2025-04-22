03cc874235d3b355a73105cadb8e551ababe6063
Cameron Showalter <cjshowalter@alaska.edu>
2025-04-22 11:04:35 -0800

    python linting changes

--- portal-cdk/portal_cdk/portal_cdk_stack.py
+++ portal-cdk/portal_cdk/portal_cdk_stack.py
@@ -8,8 +8,8 @@ from constructs import Construct
 
 class PortalCdkStack(Stack):
     def __init__(
-            self, scope: Construct, construct_id: str, maturity: str, **kwargs
-        ) -> None:
+        self, scope: Construct, construct_id: str, maturity: str, **kwargs
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
         print(f"INSIDE THE STACK: {maturity}")
         # The code that defines your stack goes here
