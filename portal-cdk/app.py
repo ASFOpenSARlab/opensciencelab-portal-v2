3d76a1b1a41639f9de7b52644e8497d28a869e18
Cameron Showalter <cjshowalter@alaska.edu>
2025-04-22 13:37:28 -0800

    No need for maturity, only have deploy_prefix

--- portal-cdk/app.py
+++ portal-cdk/app.py
@@ -7,22 +7,18 @@ import aws_cdk as cdk
 from portal_cdk.portal_cdk_stack import PortalCdkStack
 
 deploy_prefix = os.getenv("DEPLOY_PREFIX")
-
-default_maturity = deploy_prefix if deploy_prefix in ["prod", "test"] else "dev"
-maturity = os.getenv("MATURITY", default_maturity)
-
-# Default to DEPLOY_PREFIX first, then MATURITY:
-stack_id = deploy_prefix or maturity
+if not deploy_prefix:
+    raise ValueError("You forgot an env-var `make <target> -e DEPLOY_PREFIX=<you>`")
 
 app = cdk.App()
 PortalCdkStack(
     app,
-    f"PortalCdkStack-{stack_id}",
+    f"PortalCdkStack-{deploy_prefix}",
     env=cdk.Environment(
         account=os.getenv("CDK_DEFAULT_ACCOUNT"),
         region=os.getenv("CDK_DEFAULT_REGION"),
     ),
-    stack_id=stack_id,
+    deploy_prefix=deploy_prefix,
 )
 
 app.synth()
