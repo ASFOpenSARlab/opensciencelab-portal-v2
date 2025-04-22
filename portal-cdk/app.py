3b40236950f4835bf92cb2bc68c7cf283c2f55c8
Cameron Showalter <cjshowalter@alaska.edu>
2025-04-22 13:03:10 -0800

    Added support for changing BOTH maturity and deploy-prefix

--- portal-cdk/app.py
+++ portal-cdk/app.py
@@ -6,19 +6,23 @@ import aws_cdk as cdk
 
 from portal_cdk.portal_cdk_stack import PortalCdkStack
 
-# NOT using 'get' here, so that we fail fast if this isn't set.
-# (you should be using the makefile to deploy this...)
-maturity = os.environ["MATURITY"]
+deploy_prefix = os.getenv("DEPLOY_PREFIX")
+
+default_maturity = deploy_prefix if deploy_prefix in ["prod", "test"] else "dev"
+maturity = os.getenv("MATURITY", default_maturity)
+
+# Default to DEPLOY_PREFIX first, then MATURITY:
+stack_id = deploy_prefix or maturity
 
 app = cdk.App()
 PortalCdkStack(
     app,
-    f"PortalCdkStack-{maturity}",
+    f"PortalCdkStack-{stack_id}",
     env=cdk.Environment(
         account=os.getenv("CDK_DEFAULT_ACCOUNT"),
         region=os.getenv("CDK_DEFAULT_REGION"),
     ),
-    maturity=maturity,
+    stack_id=stack_id,
 )
 
 app.synth()
