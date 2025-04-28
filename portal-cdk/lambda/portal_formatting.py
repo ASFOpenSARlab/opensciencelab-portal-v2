08d5d6a0184efed6b54df32b1c8e36e51fd5a705
bbuechle <btbuechler@alaska.edu>
2025-04-28 16:17:31 -0600

    Initial attempt at basic templating formatting (v2)

--- portal-cdk/lambda/portal_formatting.py
+++ portal-cdk/lambda/portal_formatting.py
@@ -1,13 +1,20 @@
+from jinja2 import Environment, FileSystemLoader, StrictUndefined, select_autoescape
 
-
-from jinja2 import Environment, PackageLoader, select_autoescape
-
-env = Environment(
-    loader=PackageLoader("OSL Portal V2"),
-    autoescape=select_autoescape()
+from aws_lambda_powertools.event_handler import (
+    Response,
+    content_types,
 )
 
-IMPORT_PATH = 'https://opensciencelab.asf.alaska.edu/portal/hub/static/css/'
+ENV = Environment(
+    loader=FileSystemLoader("./templates/"),
+    autoescape=select_autoescape(),
+    undefined=StrictUndefined,
+    trim_blocks=True,
+    lstrip_blocks=True,
+    keep_trailing_newline=True,
+)
+
+IMPORTS_PATH = "https://opensciencelab.asf.alaska.edu/portal/hub/static/css/"
 
 NAV_BAR_OPTIONS = [
     {
@@ -42,14 +49,25 @@ NAV_BAR_OPTIONS = [
     },
 ]
 
-def portal_template( content, username="Unknown"):
 
+def basic_html(body, code=200, content_type=content_types.TEXT_HTML):
+    return Response(
+        status_code=code,
+        content_type=content_type,
+        body=body,
+        # headers=custom_headers,
+        # cookies=[Cookie(name="session_id", value="12345")],
+    )
+
+
+def portal_template(content, title="OSL Portal", username="Unknown"):
     template_input = {
         "content": content,
-        "import_path": IMPORT_PATH,
+        "imports_path": IMPORTS_PATH,
         "nav_bar_options": NAV_BAR_OPTIONS,
         "username": username,
+        "title": title,
     }
 
-    template = env.get_template("templates/main.j2")
+    template = ENV.get_template("main.j2")
     return template.render(**template_input)
