07f06784ebc17a5830eeceead4831d14e31863ec
Eric Lundell <emlundell@alaska.edu>
2025-05-01 16:23:43 -0800

    Abstract portal auth endpoints to username So we are not hardcoding auth info

--- portal-cdk/lambda/main.py
+++ portal-cdk/lambda/main.py
@@ -6,14 +6,26 @@ import json
 from http import HTTPStatus
 import datetime
 
+from opensarlab.auth import encryptedjwt
+
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
 from aws_lambda_powertools.event_handler import Response
 from aws_lambda_powertools.event_handler import content_types
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.shared.cookies import Cookie
+from aws_lambda_powertools.utilities import parameters
 
 logger = Logger(service="APP")
+
+TEMP_USERNAME = "emlundell"
+
+
+def encrypt_data(data: dict) -> str:
+    sso_token = parameters.get_secret("temp-sso-token")
+    encryptedjwt.encrypt(data, sso_token=sso_token)
+    return data
+
 
 # Rest is V1, HTTP is V2
 app = APIGatewayHttpResolver()
@@ -53,15 +65,29 @@ def get_portal_hub_auth():
 @app.post("/portal/hub/auth")
 def post_portal_hub_auth():
     logger.info("Request user info")
-    data = json.dumps(
-        {
-            "data": "gAAAAABoC9GFoQF44vqO8uIuSxnUJgblmvjZ4Cs-8EWYx1GY26b_CrY5erDOKrrzCDJTyV004IRFxAqcmAZoZHzcHLmt2bE9vZO4cwbtpYGP2uBfPjDROcOeVSgMT1Mjez-RaeYfiePmfO85S6IBJzUVGww_tsc-WnG9jQCBtO84XEhKHPmVkMgmPhVL7G6j3iG7mzcqqp0fMHr45aarNwoN1OqOpGKTiQwOdDgR0wZRKHAU7346-rMhZf9E7-hlilWnXFtGjjNEvPBHQidMSVcG5wWG8iWQK50egCxYdfwubBGXBIEZgJGgUPfp0DOgLrSUp0l23ZHqydx6j1nhdZumEfa3T8fYrtPKBANQrairQds09UTbwx0UHQ0ImpAs0uo40Zer2N_vCBzqJtHj36roiK6iGjYSDZ-fXLRa6CzfghnD8RpYU_R4k108eyyLF-yCRQCJJpuQHfGN-8S7g-uYAAk4JCa_IIZqV3vkZPzXm7Kt4CNUj6u8oJKqumQoyBt-2gNyzrYgRzgYD_cZ5_1dTAK3Td0w2Viyb_4Bv2xjjlvFeL-oIkVYoAlQ0u4S2thDE7IYwPNgjjH8Qe-BzFY48fTV48mgWoEFrrw08LQg1sif2K8YXJQwAsXMkPwvHD6S9q67EgwoCVYeMswW8LIdXG9WUOBE52xkGUwYYPjZPJPL1yLL_uEHcE4hxM4cwa9U9cD9IqRODrrs6yYdshhwacAP2BLYD2w2pvqT5ck49QCfBl1zLU2UQ01dai6vnVPCLkvJUP5g6v_C-w4ktTeDT9WUPZKnsSwZUSVjapjha2-QwdlmX3df54Nnk9ojwadaSeqe0c1KdCjO1z-5iVUdC4WsrZ6RqL6qaighMOSWRg4hP8JagHTKygLN3Jdua7wPvrmu2MGA8HDfhvWoUcR12OT6LcfAriPAifpBlgSeziVA07d-XP5XtY1HOlt0fcI7ivTfDc50lCfIU4rwp2qThfVC6H2TPbkiFUMt77XLFi7QYs9NfrK9uSdFBpBxSJ2_Q7-Uewfl1qBOuEcWU4F_1SW_7KXBhLBwf-maaxwzjQrJMGSNXgzVJyjFBSrTTm1TYoxZs6mAIWhbkdCJS5SYmz6tTacTY98bZvrPUDHhdhATt0iqCV79Tdj17CbBlR9dLrSrRhOAkxxf8EOgyeTa4jjtAbZdvw==",
-            "message": "OK",
-        }
-    )
+    data = {
+        "admin": True,
+        "roles": ["user", "admin"],
+        "name": f"{TEMP_USERNAME}",
+        "has_2fa": 1,
+        "force_user_profile_update": False,
+        "ip_country_status": "unrestricted",
+        "country_code": "US",
+        "lab_access": {
+            "smce-test-opensarlab": {
+                "lab_profiles": ["m6a.large"],
+                "time_quota": None,
+                "lab_country_status": "unrestricted",
+                "can_user_access_lab": True,
+                "can_user_see_lab_card": True,
+            },
+        },
+    }
+    encrypted_data = encrypt_data(data)
+    body = json.dumps({"data": encrypted_data, "message": "OK"})
     return Response(
         status_code=HTTPStatus.OK.value,  # 200
-        body=data,
+        body=body,
     )
 
 
@@ -69,7 +95,7 @@ def post_portal_hub_auth():
 def portal_hub_login():
     logger.info("Log in user")
     cookie_name = "portal-username"
-    cookie_value = '"gAAAAABoEYMwkZJeiJwc3rFWmkUzhV_DmHb5aWyQt6VcPvME2MVIjlRPnl6v3oyAs-kY6yt_grfN5yCNmudqs8MWwQZKWq6MFwPUV5sQoW1deAdYQCqR4dqtxih5qk3s_x4be1q80a_VQADdULJcwVjtMb6R4WDiooIVA0xpqg7jI-i3AmbQgWAFfo-jFNFSPqG1qUmc-tXn_EmfTMqIzffgJUVgQKEtDw=="'
+    cookie_value = encrypt_data(TEMP_USERNAME)
     expiration_date = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
         days=7
     )
