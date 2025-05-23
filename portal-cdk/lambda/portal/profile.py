cc70ab7be3d31147033d5fcb76b064841a5fca2c
segallagher <segallagher@alaska.edu>
2025-05-23 13:59:02 -0800

    feat: introduce profile post form processing

--- portal-cdk/lambda/portal/profile.py
+++ portal-cdk/lambda/portal/profile.py
@@ -1,9 +1,18 @@
-from util.format import portal_template
+from util.format import (
+    portal_template,
+    render_template,
+)
 from util.auth import require_access, get_user_from_event
+from util.dynamo_db import update_item
+from util.responses import wrap_response
 from pathlib import Path
 import json
+from base64 import b64decode
+from urllib.parse import parse_qs
+from typing import Any
 
 from aws_lambda_powertools import Logger
+from aws_lambda_powertools.event_handler import content_types
 from aws_lambda_powertools.event_handler.api_gateway import Router
 
 logger = Logger(service="APP", level="DEBUG")
@@ -54,3 +63,85 @@ def profile_user(user):
     with open(CWD / "../data/countries.json", "r") as f:
         page_dict["input"]["countries"] = json.loads(f.read())
     return page_dict
+
+
+def process_profile_form(request_body:str) -> tuple[bool, dict[str, Any]]:
+    """Processes the profile form
+
+    Args:
+        request_body (str): Base64 encoded request body
+
+    Returns:
+        tuple[bool, dict[str, Any]]: _description_
+    """
+    decoded_body = b64decode(request_body)
+    parsed_qs = parse_qs(decoded_body, keep_blank_values=True)
+    query_dict: dict[str, Any] = {k.decode('utf-8'):v[0].decode('utf-8') for k,v in parsed_qs.items()}
+    
+    # Validate Form
+    ## TODO
+    correct=True
+    if not correct:
+        return False, {}
+    
+    ## Redirect to profile if not filled out correctly
+    
+    # Format checkboxes
+    checkbox_fields = [
+        "faculty_member_affliated_with_university",
+        "research_member_affliated_with_university",
+        "graduate_student_affliated_with_university",
+        ]
+    
+    for field in checkbox_fields:
+        if field in query_dict:
+            query_dict[field] = True
+        else:
+            query_dict[field] = False
+            
+    # Format yes/no to Bool values
+    for k,v in query_dict.items():
+        if v == "yes":
+            query_dict[k] = True
+        if v == "no":
+            query_dict[k] = False
+        # If value is "default" then it should not be important at this stage since the form is validated
+        if v == "default":
+            query_dict[k] = False
+    
+    # Return dictionary of values
+    return True, query_dict
+    
+    
+
+@profile_router.post("/<user>")
+@require_access()
+@portal_template(profile_router, name="profile.j2")
+def profile_user_filled(user):
+    # Parse form request
+    body = profile_router.current_event.body
+    success, query_dict = process_profile_form(body)
+    
+    if success:        
+        # Update user profile
+        print(query_dict)
+        update_item(user, {"profile": query_dict})
+        print("PROFILE UPDATED")
+
+        # Send the user to the portal
+        next_url = "/portal"
+        return wrap_response(
+            body={"Redirect": next_url},
+            code=302,
+            content_type=content_types.APPLICATION_JSON,
+            headers={"Location": next_url},
+        )
+    else:
+        # Send the user back to the profile page
+        next_url = "/portal/profile/<user>"
+        return wrap_response(
+            body={"Redirect": next_url},
+            code=302,
+            content_type=content_types.APPLICATION_JSON,
+            headers={"Location": next_url},
+        )
