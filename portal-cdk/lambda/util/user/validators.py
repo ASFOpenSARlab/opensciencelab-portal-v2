4b2f8a8b6dde801b18a46e806a3c62baccd57cdf
Cameron Showalter <cjshowalter@alaska.edu>
2025-05-28 13:02:42 -0800

    Linting changes

--- portal-cdk/lambda/util/user/validators.py
+++ portal-cdk/lambda/util/user/validators.py
@@ -1,5 +1,6 @@
 """A list of validators, for custom parsing input data."""
 
+
 def dict_contains_random_key(validate_dict):
     validate_dict = dict(validate_dict)
     if "random" not in validate_dict:
