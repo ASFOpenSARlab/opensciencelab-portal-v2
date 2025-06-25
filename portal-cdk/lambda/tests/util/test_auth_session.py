969a34c50ae5031be0fc09f642a77ee6191b9e69
Cameronsplaze <cjshowalter@alaska.edu>
2025-06-25 12:55:56 -0800

    Python formatting changes from action

--- portal-cdk/lambda/tests/util/test_auth_session.py
+++ portal-cdk/lambda/tests/util/test_auth_session.py
@@ -1,4 +1,3 @@
-
 import main
 
 
@@ -6,11 +5,14 @@ class TestAuthSession:
     def setup_method(self, _method):
         # We have to clear current_session, so it's reset between each test:
         from util.session import current_session
+
         current_session.auth = None
         current_session.user = None
         current_session.app = None
 
-    def test_session_user_saved(self, lambda_context, monkeypatch, helpers, fake_auth, mocked_requests_post):
+    def test_session_user_saved(
+            self, lambda_context, monkeypatch, helpers, fake_auth, mocked_requests_post
+        ):
         # Create FakeUser instance to be monkeypatched in and inspected after modified
         user = helpers.FakeUser()
         monkeypatch.setattr("main.User", lambda *args, **kwargs: user)
@@ -23,6 +25,7 @@ class TestAuthSession:
             lambda a: "er9LnqEOiH+JLBsFCy0kVeba6ZSlG903cliU7VYKnM8=",
         )
         from util.session import current_session
+
         event = helpers.get_event(
             path="/auth",
             qparams={
@@ -36,7 +39,9 @@ class TestAuthSession:
         assert current_session.user is not None, "User should be set after handler"
         assert current_session.user.username == "test_user", "Username should be set to 'test_user'"
 
-    def test_session_no_cookie(self, lambda_context, monkeypatch, helpers, mocked_requests_post):
+    def test_session_no_cookie(
+            self, lambda_context, monkeypatch, helpers, mocked_requests_post
+        ):
         # Create FakeUser instance to be monkeypatched in and inspected after modified
         user = helpers.FakeUser()
         monkeypatch.setattr("main.User", lambda *args, **kwargs: user)
@@ -49,6 +54,7 @@ class TestAuthSession:
             lambda a: "er9LnqEOiH+JLBsFCy0kVeba6ZSlG903cliU7VYKnM8=",
         )
         from util.session import current_session
+
         event = helpers.get_event(
             path="/auth",
             qparams={
@@ -58,9 +64,13 @@ class TestAuthSession:
         )
         assert current_session.user is None, "User should not be set before handler"
         main.lambda_handler(event, lambda_context)
-        assert current_session.user is None, "User should still be None without a auth cookie"
+        assert current_session.user is None, (
+            "User should still be None without a auth cookie"
+        )
 
-    def test_session_user_overridden(self, lambda_context, monkeypatch, helpers, mocked_requests_post):
+    def test_session_user_overridden(
+            self, lambda_context, monkeypatch, helpers, mocked_requests_post
+        ):
         # Create FakeUser instance to be monkeypatched in and inspected after modified
         user = helpers.FakeUser()
         monkeypatch.setattr("main.User", lambda *args, **kwargs: user)
@@ -73,6 +83,7 @@ class TestAuthSession:
             lambda a: "er9LnqEOiH+JLBsFCy0kVeba6ZSlG903cliU7VYKnM8=",
         )
         from util.session import current_session
+
         # Pretend this user is from a previous warm-start call:
         current_session.user = user
         # And this should override it, even if there's no cookie:
@@ -85,4 +96,6 @@ class TestAuthSession:
         )
         assert current_session.user == user, "User before handler should be generic"
         main.lambda_handler(event, lambda_context)
-        assert current_session.user is None, "User should still be None without a auth cookie"
+        assert current_session.user is None, (
+            "User should still be None without a auth cookie"
+        )
