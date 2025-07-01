89bb3e01dc3c59ea9aa05dd6a6b52fdef666fad5
Cameronsplaze <cjshowalter@alaska.edu>
2025-07-01 13:34:40 -0800

    Added second lambda, so we can validate usernames before accepting them in cognito

---
+++ portal-cdk/lambda_signup/main.py
@@ -0,0 +1,17 @@
+import re
+
+## Keep both these here, so the test suite can use them too:
+#   The 'r' string is to not escape the backslashes
+USERNAME_REGEX = r"^[a-zA-Z0-9\._-]+$"
+#   The '- ' to separate this from the previous message in the cognito login UI.
+#     (And the message already ends in a period.)
+ERROR_MESSAGE = (
+    "- Username contains invalid special characters. "
+    "Only periods, underscores, and dashes allowed"
+)
+
+def lambda_handler(event, _context):
+    if re.match(USERNAME_REGEX, event['userName']) is None:
+        raise ValueError(ERROR_MESSAGE)
+    # Return to Amazon Cognito
+    return event
