e9af3030702d074c7807aad1f2016b1a6a4a3617
Cameronsplaze <cjshowalter@alaska.edu>
2025-07-01 13:57:57 -0800

    Realized escaping the - would cause less confusion later, in case a character was placed after it

--- portal-cdk/lambda_signup/main.py
+++ portal-cdk/lambda_signup/main.py
@@ -2,7 +2,8 @@ import re
 
 ## Keep both these here, so the test suite can use them too:
 #   The 'r' string is to not escape the backslashes
-USERNAME_REGEX = r"^[a-zA-Z0-9\._-]+$"
+#   Best regex viewer: https://regexr.com/
+USERNAME_REGEX = r"^[a-zA-Z0-9\._\-]+$"
 #   The '- ' to separate this from the previous message in the cognito login UI.
 #     (And the message already ends in a period.)
 ERROR_MESSAGE = (
