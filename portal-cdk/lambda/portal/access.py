77508560232aa9582736db4119fdbd02ccc36918
segallagher <segallagher@alaska.edu>
2025-07-01 13:35:43 -0800

    feat: add lab class, barebone lab display and barebone lab management

--- portal-cdk/lambda/portal/access.py
+++ portal-cdk/lambda/portal/access.py
@@ -1,5 +1,7 @@
 from util.format import portal_template
 from util.auth import require_access
+from util.user.dynamo_db import list_users_with_lab
+from labs import labs_dict
 
 from aws_lambda_powertools.event_handler.api_gateway import Router
 
@@ -17,7 +19,7 @@ access_route = {
 @require_access()
 @portal_template()
 def access_root():
-    return "List All Labs"
+    return "Access Labs"
 
 
 @access_router.get("/add_lab")
@@ -25,6 +27,25 @@ def access_root():
 @portal_template()
 def add_lab():
     return "Create New Lab"
+
+
+@access_router.get("/manage/<shortname>")
+@require_access()
+@portal_template(name="manage.j2")
+def manage_lab(shortname):
+    page_dict = {
+        "content": "Manage Lab",
+        "input": {
+        },
+    }
+    
+    lab = labs_dict[shortname]
+    page_dict["input"]["lab"] = lab
+    
+    users = list_users_with_lab(lab.short_lab_name)
+    page_dict["input"]["users"] = users
+    
+    return page_dict
 
 
 @access_router.get("/lab/<lab>")
