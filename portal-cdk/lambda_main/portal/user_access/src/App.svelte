2054f4f3ad61296fe3ef556ed34d7d845a1a83f5
Eric Lundell <emlundell@alaska.edu>
2025-07-22 16:02:43 -0800

    Better alignment. Userdata icons.

--- portal-cdk/lambda_main/portal/user_access/src/App.svelte
+++ portal-cdk/lambda_main/portal/user_access/src/App.svelte
@@ -1,12 +1,16 @@
 <script>
   import { onMount } from 'svelte';
   import UserInfo from './lib/UserInfo.svelte'
-  import { Button, Input, ListGroup, ListGroupItem, Spinner, TabContent, TabPane } from '@sveltestrap/sveltestrap';
+  import { Button, Icon, Input, ListGroup, ListGroupItem, Spinner, TabContent, TabPane } from '@sveltestrap/sveltestrap';
 
-  let usernames = $state([]);
-  let selectedUsername = $state("emlundell");
+  // sm}psmnpmnpsml{mms{ppmpnm{plmsmpps{pllp{p{npsmpp
+  let usernames = $state(["emlundell"]);
   let searchTerm = $state("");
   let filteredUsernames = $derived(usernames.filter(username => username.indexOf(searchTerm) !== -1));
+  let selectedUsername = $state("emlundell");
+  let selectedChildData = $state({});
+
+  let setUsernameData = (childData) => { selectedChildData = childData; console.log($state.snapshot(selectedChildData))}
 
   onMount(async () => {
       const url = '/portal/users/all/usernames'
@@ -21,6 +25,7 @@
 
 <svelte:head>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.css">
 </svelte:head>
 
 <main>
@@ -37,13 +42,26 @@
     <div id="scrollarea-div">
       <ListGroup>
         {#each filteredUsernames as username}
-          <ListGroupItem onclick={() => selectedUsername = username}> { username } </ListGroupItem>
+          <ListGroupItem onclick={() => selectedUsername = username}> 
+            <p class="scrollarea-p" id="scrollarea-p-{ username }"> 
+              <span> { username } </span>
+            </p>
+          </ListGroupItem>
         {/each}
       </ListGroup>
     </div>
 
     <div id="username-div">
-      <p>{selectedUsername}</p>
+      <p> 
+        {selectedUsername}
+        &nbsp;
+        {#if selectedChildData.is_locked == true }
+          <Icon name="lock" />
+        {/if}
+        {#if [selectedChildData.roles].includes('admin') }
+          <Icon name="person-rolodex" />
+        {/if}
+      </p>
     </div>
 
     <div id="tabs-div">
@@ -51,7 +69,7 @@
         <TabContent>
           <TabPane tabId="userInfo" tab="User Info" active>
             {#key selectedUsername}
-              <UserInfo username={ selectedUsername }/>
+              <UserInfo username={ selectedUsername } { setUsernameData } />
             {/key}
           </TabPane>
           <TabPane tabId="labInfo" tab="Lab Info">
@@ -73,7 +91,7 @@
     align-items: center;
     row-gap: 20px;
     column-gap: 50px;
-    grid-template-columns: 400px 200px auto;
+    grid-template-columns: 300px 80% auto;
     grid-template-rows: 50px 50px minmax(50rem, 100%);
     grid-template-areas: 
       "btn . ."
@@ -95,9 +113,11 @@
   }
 
   #username-div > p {
-    margin: 0;
+    margin-bottom: 1rem;
     text-align: left;
     font-size: 3rem;
+    text-overflow: ellipsis;
+    overflow: hidden;
   }
 
   #scrollarea-div {
@@ -106,9 +126,14 @@
     height: 100%;
   }
 
+  .scrollarea-p {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    text-align: left;
+  }
+
   #tabs-div {
     grid-area: tabs;
-    border: 3px solid yellow;
     height: 100%;
     width: 100%;
   }
--- portal-cdk/lambda_main/portal/user_access/src/lib/UserInfo.svelte
+++ portal-cdk/lambda_main/portal/user_access/src/lib/UserInfo.svelte
@@ -1,28 +1,16 @@
 <script>
     import { Button } from '@sveltestrap/sveltestrap';
 
-    let { username } = $props()
+    let { username, setUsernameData } = $props()
     let userData = $state()
 
     console.log("prop 'username' selected: " + username)
 
-    if (username) {
-        const url = '/portal/users/get/'+username;
-
-        fetch(url, {
-            method: 'GET',
-        })
-        .then( response => response.json() )
-        .then( data => { userData = data } )
-        .catch( error => { console.log(error)} )
-    }
-
-    
     userData = {
         "last_cookie_assignment": "2025-07-14 23:11:13",
         "created_at": "2025-06-03 07:12:32",
         "last_update": "2025-07-14 23:11:13",
-        "labs": [], 
+        "labs": ["qwerty"], 
         "profile": {
             "user_affliated_with_nasa_research_email": "", 
             "pi_affliated_with_nasa_research_email": "", 
@@ -42,12 +30,23 @@
         "random_dict": null, 
         "email": "emlundell@alaska.edu", 
         "roles": ["user"], 
-        "is_locked": false, 
+        "is_locked": true, 
         "some_int_without_default": null, 
         "username": "emlundell", 
         "some_int_with_default": "42", 
         "require_profile_update": false
     };
+
+    if (username) {
+        const url = '/portal/users/get/'+username;
+
+        fetch(url, {
+            method: 'GET',
+        })
+        .then( response => response.json() )
+        .then( data => { userData = data; setUsernameData(userData) } )
+        .catch( error => { console.log(error); setUsernameData(userData)} )
+    }
       
 </script>
 
@@ -56,10 +55,10 @@
     <div class="container">
         {#if userData }
             <div id="username-div">
-                <p>Username: { username }</p>
+                <p><b>Username:</b> { username }</p>
             </div>
             <div id="createdTime-div">
-                <p>Created: { userData.created_at }</p>
+                <p>Created: <i>{ userData.created_at }</i></p>
             </div>
             <div id="updatedTime-div">
                 <p>Updated: { userData.last_update }</p>
@@ -93,6 +92,7 @@
 
 <style>
     .container {
+        border: 1px solid grey;
         display: grid;
         text-align: left;
         row-gap: 10px;
@@ -100,15 +100,19 @@
         grid-template-rows: auto auto auto auto auto auto minmax(10rem, 100%);
         grid-template-columns: auto auto auto;
         grid-template-areas:
-            "username . actionBtns"
+            "username username actionBtns"
             "email . ."
             "createdTime updatedTime ."
             "ipAddr ipCountry ."
-            "access . ."
-            "labs . ."
+            "access labs labs"
             "profileEditBtn commentsEditBtn ."
             "profile profile profile" 
             "comments comments .";
+    }
+
+    .container > div {
+        margin: 0.5rem;
+        padding: 0.5rem;
     }
 
     #username-div {
@@ -143,5 +147,6 @@
 
     #actionBtns-div {
         grid-area: actionBtns;
+        justify-self: end;
     }
 </style>
