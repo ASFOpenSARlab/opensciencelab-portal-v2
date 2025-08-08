d537d8c4d5be0782c70e155177d0c94d74fc8504
segallagher <segallagher@alaska.edu>
2025-08-07 16:28:03 -0800

    chore: messy push for additional eyes

--- portal-cdk/lambda_main/util/user/user.py
+++ portal-cdk/lambda_main/util/user/user.py
@@ -5,7 +5,7 @@ import datetime
 import frozendict
 from typing import Any
 
-from util.exceptions import DbError, CognitoError
+from util.exceptions import DbError, CognitoError, UserCreationError
 from util.cognito import delete_user_from_user_pool
 from util.labs import all_labs, LabAccessInfo, BaseLab
 
@@ -63,13 +63,34 @@ def create_lab_structure(
 
 
 class User:
-    def __init__(self, username: str):
+    def __init__(self, username: str, create_if_missing:bool=True):
+        with open("test.txt", 'a') as f:
+            f.write("starting, ")
         ## Using super to avoid setattr validation. 'username'
         #  should NOT be modified like the other attributes.
         super().__setattr__("username", username)
 
         ## Apply anything in the DB:
         db_info = get_item(self.username)
+        
+        with open("test.txt", 'a') as f:
+            f.write(f"{username}, ")
+            f.write(f"{create_if_missing}")
+        print(f"DBINFO: {db_info}")
+        if db_info:
+            with open("test.txt", 'a') as f:
+                f.write(", grabble\n")
+        else:
+            with open("test.txt", 'a') as f:
+                f.write("\n")
+            
+            
+        if not create_if_missing:
+            raise UserCreationError(f"User {self.username} does not exist and was not created")
+        ## If it doesn't exist and not creating new one, raise error
+        if not db_info and not create_if_missing:
+            raise UserCreationError(f"User {self.username} does not exist and was not created")
+        
         ## If it doesn't exist, create it with the defaults:
         if not db_info:
             create_item(self.username, defaults)
