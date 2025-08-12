a6a2c8aebc111fefadbca95cd8fd1e578a973887
segallagher <segallagher@alaska.edu>
2025-08-12 15:26:55 -0800

    feat: add endpoint to get a users labs

--- portal-cdk/lambda_main/util/user/user.py
+++ portal-cdk/lambda_main/util/user/user.py
@@ -13,7 +13,7 @@ from .dynamo_db import get_item, create_item, update_item, delete_item
 from .defaults import defaults
 from .validator_map import validator_map, validate
 
-
+# filters all_labs by which labs a user has access to
 def filter_lab_access(
     is_admin: bool, all_labs_in: dict[str, BaseLab], labs: dict
 ) -> list[LabAccessInfo]:
