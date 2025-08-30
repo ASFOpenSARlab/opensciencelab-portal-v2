6b8b1fa0319946a831d49f270c39ca1f641c885e
Eric Lundell <emlundell@alaska.edu>
2025-08-29 21:18:31 -0800

    Add placeholder header, footer, get all usernames It works locally but not remotely. Not sure why.

--- portal-cdk/svelte/src/routes/+page.svelte
+++ portal-cdk/svelte/src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { username } from "$lib/utils";
+  import { page } from "$app/state";
+
+  let domain = page.url.host;
+  let usernames = $state([]);
+
+  async function getUsernames() {
+    const url = domain + "/portal/users/all/usernames";
+
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((response) => {
+        console.log("Response: ", response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Data: ", data);
+        if (Object.keys(data).length === 0) {
+          throw "No data";
+        }
+        usernames = data;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
+</script>
+
+<div>
+  <h1>Welcome {username}</h1>
+
+  <p>Here's a list of all usernames:</p>
+
+  <div class="container">
+    {#await getUsernames()}
+      <!--Spinner type="border" size="" color="primary" /-->
+      Loading...
+    {:then data}
+      {#each Object.entries(usernames) as [key, username]}
+        <p>{username}</p>
+      {/each}
+    {:catch error}
+      <h3 class="color-error">{error}</h3>
+    {/await}
+  </div>
+</div>
