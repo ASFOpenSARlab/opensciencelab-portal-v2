803bbc92e847792db780976c991fecc6d17097c9
Cameronsplaze <cjshowalter@alaska.edu>
2025-09-16 14:51:27 -0800

    Added SES support. Mainly loading an external SES from the stack, and giving lambda permissions to send emails from it. Then hooking Cognito to use it as well

--- portal-cdk/app.py
+++ portal-cdk/app.py
@@ -6,19 +6,21 @@ import aws_cdk as cdk
 
 from portal_cdk.portal_cdk_stack import PortalCdkStack
 
-deploy_prefix = os.getenv("DEPLOY_PREFIX")
-if not deploy_prefix:
-    raise ValueError("You forgot an env-var `make <target> -e DEPLOY_PREFIX=<you>`")
+required_vars = {}
+for var in ["DEPLOY_PREFIX", "SES_DOMAIN", "SES_REPLY_TO_EMAIL"]:
+    if not os.getenv(var):
+        raise ValueError(f"You forgot an env-var `make <target> -e {var}=<value>`")
+    required_vars[var.lower()] = os.getenv(var)
 
 app = cdk.App()
 PortalCdkStack(
     app,
-    f"PortalCdkStack-{deploy_prefix}",
+    f"PortalCdkStack-{required_vars['deploy_prefix']}",
     env=cdk.Environment(
         account=os.getenv("CDK_DEFAULT_ACCOUNT"),
         region=os.getenv("CDK_DEFAULT_REGION"),
     ),
-    deploy_prefix=deploy_prefix,
+    vars=required_vars,
 )
 
 app.synth()
