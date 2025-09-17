26f7cfe00fab202583ecae0b1c27c4c77f767adc
Cameronsplaze <cjshowalter@alaska.edu>
2025-09-17 13:05:34 -0800

    Adding two email endpoints. One for backwards compatability, and one to hold the logic

--- portal-cdk/lambda_main/util/responses.py
+++ portal-cdk/lambda_main/util/responses.py
@@ -9,6 +9,9 @@ from aws_lambda_powertools.event_handler import (
     content_types,
 )
 
+from util.exceptions import MalformedRequest
+
+
 logger = Logger(child=True)
 
 
@@ -36,7 +39,22 @@ def wrap_response(body, code=200, content_type=None, headers=None, cookies=None)
 
     return Response(**response_payload)
 
+def json_body_to_dict(body: str) -> dict:
+    """Converts a JSON body to a python dictionary
 
+    Args:
+        body (str): Body of a JSON request
+
+    Returns:
+        dict: Python dictionary with key values provided from body
+    """
+    try:
+        return json.loads(body)
+    except json.JSONDecodeError as e:
+        raise MalformedRequest(
+            message="Malformed JSON",
+            extra_info={"error": str(e), "body": body},
+        ) from e
 def form_body_to_dict(body: str) -> dict:
     """Converts an html form body to a python dictionary
 
