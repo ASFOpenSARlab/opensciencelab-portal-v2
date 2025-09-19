76d30bc5955b65847336b427993bfd59aecebd09
Eric Lundell <emlundell@alaska.edu>
2025-09-18 17:15:06 -0800

    Better spinner. POSt endpoint for whoami to play with.

--- portal-cdk/svelte/src/components/layout/header.svelte
+++ portal-cdk/svelte/src/components/layout/header.svelte
@@ -1,17 +1,25 @@
 <script>
   import { base } from "$app/paths";
+  import Spinner from "$components/Spinner.svelte";
   import { UserClass } from "$lib/store.svelte.js";
   import { onMount } from "svelte";
 
+  let spinner;
   let user = new UserClass();
 
   onMount(async () => {
     user.pull();
+    // spinner is already spinning on render
+    spinner.stop();
   });
 </script>
 
+<Spinner bind:this={spinner} />
+
 <div>
-  <h1>This is a header for <i>{user.data}</i></h1>
+  <h1>
+    This is a header: <i>{(user.data && user.data.username) || "username"}</i>
+  </h1>
 </div>
 
 <style>
