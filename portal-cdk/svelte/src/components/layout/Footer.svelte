e88b5577d859ace0fe084f741add326b18dfcd08
Eric Lundell <emlundell@alaska.edu>
2025-09-23 10:33:33 -0800

    Better footer example with images

--- portal-cdk/svelte/src/components/layout/Footer.svelte
+++ portal-cdk/svelte/src/components/layout/Footer.svelte
@@ -1,12 +1,33 @@
 <div class="footer">
-  <h2>This is a footer</h2>
+  <span class="this-is-a">This is a footer</span>
+  <img
+    class="image"
+    alt="OSL iamge"
+    src="/ui/assets/images/OpenScienceLab_logo.svg"
+  />
+  <img
+    class="image"
+    alt="NASA meatball"
+    src="/ui/assets/images/NASA_logo.svg"
+  />
 </div>
 
 <style>
+  .image {
+    height: 80%;
+    float: right;
+    margin: 10px;
+  }
+
+  .this-is-a {
+    font-size: 2rem;
+    font-style: oblique;
+  }
+
   .footer {
     text-align: center;
     width: 100%;
-    height: 4rem;
+    height: 100%;
     background-color: orange;
   }
 </style>
--- portal-cdk/svelte/src/components/layout/Header.svelte
+++ portal-cdk/svelte/src/components/layout/Header.svelte
@@ -17,16 +17,21 @@
 <Spinner bind:this={spinner} />
 
 <div class="header">
-  <h2>
+  <div class="this-is-a">
     This is a header: <i>{(user.data && user.data.username) || "username"}</i>
-  </h2>
+  </div>
 </div>
 
 <style>
+  .this-is-a {
+    font-size: 2rem;
+    font-style: oblique;
+  }
+
   .header {
     text-align: center;
     width: 100%;
-    height: 3rem;
+    height: 100%;
     background-color: orange;
   }
 </style>
