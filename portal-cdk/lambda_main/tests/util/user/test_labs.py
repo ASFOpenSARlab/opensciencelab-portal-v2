0e1b88f884a6db10c31db579bf4557fd890f33d9
segallagher <segallagher@alaska.edu>
2025-09-24 15:32:23 -0800

    refactor: remove unused helpers

--- portal-cdk/lambda_main/tests/util/user/test_labs.py
+++ portal-cdk/lambda_main/tests/util/user/test_labs.py
@@ -1,7 +1,7 @@
 import importlib
 
 class TestLabs:
-    def test_lab_conditional_non_prod(self, monkeypatch, helpers):
+    def test_lab_conditional_non_prod(self, monkeypatch):
       monkeypatch.setenv("IS_PROD", "false")
       import util.labs
       importlib.reload(util.labs)
@@ -9,7 +9,7 @@ class TestLabs:
       assert "smce-test-opensarlab" in util.labs.LABS.keys()
       assert "smce-prod-opensarlab" not in util.labs.LABS.keys()
       
-    def test_lab_conditional_prod(self, monkeypatch, helpers):
+    def test_lab_conditional_prod(self, monkeypatch):
       monkeypatch.setenv("IS_PROD", "true")
       
       import util.labs
