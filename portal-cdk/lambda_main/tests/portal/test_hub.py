1dfdf394d361f1a5dd597dde8ded5da40579780e
Eric Lundell <emlundell@alaska.edu>
2025-09-30 15:54:31 -0800

    Setup mock email verification

--- portal-cdk/lambda_main/tests/portal/test_hub.py
+++ portal-cdk/lambda_main/tests/portal/test_hub.py
@@ -1,8 +1,23 @@
+import os
+
+import boto3
+from moto import mock_aws
+
 from util.auth import encrypt_data
 import main
 
+REGION = os.getenv("STACK_REGION", "us-west-2")
+EMAIL_DOMAIN = "opensciencelab.asf.alaska.edu"
 
+
+@mock_aws
 class TestHubPages:
+    def setup_method(self, method):
+        # Create verified domain
+        ses: boto3.Client = boto3.client("ses", region_name=REGION)
+        ses.verify_email_identity(EmailAddress="test_user@user.com")
+        ses.verify_domain_dkim(Domain=EMAIL_DOMAIN)
+
     def test_hub_send_email(self, monkeypatch, lambda_context, helpers, fake_auth):
         user = helpers.FakeUser()
         user_email = user.email
