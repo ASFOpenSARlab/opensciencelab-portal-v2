79ace75c8a0a37f6011cff85f8efa874650e728e
Eric Lundell <emlundell@alaska.edu>
2025-10-01 14:11:17 -0800

    Move email templates into jinja. And rest of code under utils.

--- portal-cdk/lambda_main/portal/hub.py
+++ portal-cdk/lambda_main/portal/hub.py
@@ -1,15 +1,13 @@
 import datetime
-import os
 
 import json
 import base64
 
-import boto3
-
 from util import swagger
+from util import send_email
 from util.responses import wrap_response
 from util.format import portal_template, request_context_string
-from util.auth import encrypt_data, require_access, decrypt_data
+from util.auth import encrypt_data, require_access
 from util.session import current_session
 from util.user import User
 
@@ -30,15 +28,6 @@ hub_route = {
 }
 
 logger = Logger(child=True)
-
-_sesv2 = None
-
-
-def get_sesv2() -> None:
-    global _sesv2
-    if not _sesv2:
-        _sesv2 = boto3.client("sesv2", region_name=os.environ.get("region"))
-    return _sesv2
 
 
 @hub_router.get("/", include_in_schema=False)
@@ -190,128 +179,6 @@ swagger_email_options = {
 }
 
 
-def _get_user_email_for_username(username: str) -> str:
-    if not username:
-        return None
-
-    # Since osl-admin is a special username, make sure we override with the admin email
-    if username == "osl-admin":
-        return os.getenv("SES_EMAIL")
-
-    try:
-        user = User(username, create_if_missing=False)
-    except Exception:
-        logger.error(f"User {username} not found")
-        return None
-
-    return user.email
-
-
-def _parse_email_message(data: dict) -> dict:
-    """
-    Parse sent POST payload into a dictionary of email parameters
-
-    Return dict of form:
-
-    {
-        "from": ["",],
-        "to": ["",],
-        "reply_to": ["",],
-        "cc": ["",],
-        "bcc": ["",],
-        "subject": "",
-        "html_body": ""
-    }
-
-    """
-
-    email_meta = {}
-
-    ####  To
-    to_email = data["to"].get("email", [])
-    if isinstance(to_email, str):
-        to_email = [to_email]
-
-    to_username = data["to"].get("username", [])
-    if isinstance(to_username, str):
-        to_username = [to_username]
-    for user in to_username:
-        user_email = _get_user_email_for_username(username=user)
-        if user_email:
-            to_email.append(user_email)
-
-    if not to_email:
-        raise Exception("No TO user specified")
-
-    email_meta["to"] = to_email
-
-    ####  CC
-    cc = data.get("cc", None)
-    if cc:
-        cc_email = cc.get("email", [])
-        if isinstance(cc_email, str):
-            cc_email = [cc_email]
-
-        cc_username = cc.get("username", [])
-        if isinstance(cc_username, str):
-            cc_username = [cc_username]
-        for user in cc_username:
-            user_email = _get_user_email_for_username(username=user)
-            if user_email:
-                cc_email.append(user_email)
-
-        email_meta["cc"] = cc_email
-
-    ####  BCC
-    bcc = data.get("bcc", None)
-    if bcc:
-        bcc_email = bcc.get("email", [])
-        if isinstance(bcc_email, str):
-            bcc_email = [bcc_email]
-
-        bcc_username = bcc.get("username", [])
-        if isinstance(bcc_username, str):
-            bcc_username = [bcc_username]
-
-        for user in bcc_username:
-            user_email = _get_user_email_for_username(username=user)
-            if user_email:
-                bcc_email.append(user_email)
-
-        email_meta["bcc"] = bcc_email
-
-    ####  FROM
-    ## It will be assumed that all emails will be FROM only one user and one REPLY-TO.
-    ## Therefore the FROM will always be overriden and all inputed FROM parameters will be ignored.
-    email_meta["reply_to"] = [os.getenv("SES_EMAIL")]
-    email_meta["from"] = f'"OpenScienceLab" <admin@{os.getenv("SES_DOMAIN")}>'
-
-    #### subject
-    data_subject = data.get("subject", "")
-    if data_subject:
-        email_meta["subject"] = data_subject
-
-    data_body = data.get("html_body", "")
-
-    if data_body:
-        email_meta["html_body"] = f"""
-        <html>
-            <body>
-                <h1> Message from OpenScienceLab </h1>
-                <section>
-                    {data_body}
-                </section>
-                <section>
-                    <hr/>
-                    <p> OpenScienceLab is operated by the Alaska Satellite Facility | <a href="https://opensarlab-docs.asf.alaska.edu">OpenScienceLab documentation</a>.</p>
-                </section>
-            </body>
-        </html>
-        """
-
-    return email_meta
-
-
 @hub_router.post(
     "/user/email",
     **swagger_email_options,
@@ -351,81 +218,9 @@ Format of `POST` payload should be a dict of the form:
 """,
 )
 def send_user_email():
-    sesv2: boto3.Client = get_sesv2()
+    request_data = hub_router.current_event.body
 
-    try:
-        request_data = hub_router.current_event.body
-
-        decrypted_data: dict = decrypt_data(request_data)
-
-        parsed_data: dict = _parse_email_message(decrypted_data)
-
-        sesv2.send_email(
-            FromEmailAddress=parsed_data.get("from", ""),
-            Destination={
-                "ToAddresses": parsed_data.get("to", []),
-                "CcAddresses": parsed_data.get("cc", []),
-                "BccAddresses": parsed_data.get("bcc", []),
-            },
-            ReplyToAddresses=parsed_data.get("reply_to", []),
-            Content={
-                "Simple": {
-                    "Subject": {
-                        "Data": parsed_data.get("subject", ""),
-                        "Charset": "UTF-8",
-                    },
-                    "Body": {
-                        "Html": {
-                            "Data": parsed_data.get("html_body", ""),
-                            "Charset": "UTF-8",
-                        },
-                    },
-                },
-            },
-        )
-
-        result = "Success"
-    except Exception as e:
-        logger.error(f"Could not send email: {e}")
-        logger.info("Sending admin error email...")
-
-        html_body = f"""
-        <html>
-            <body>
-                <h1> Message from OpenScienceLab </h1>
-                <section>
-                    <p>There was an error while sending an email...</p>
-                </section>
-                <section>
-                    {e}
-                </section>
-            </body>
-        </html>
-        """
-
-        sesv2.send_email(
-            FromEmailAddress=f'"OpenScienceLab" <admin@{os.getenv("SES_DOMAIN")}>',
-            Destination={
-                "ToAddresses": [os.getenv("SES_EMAIL")],
-            },
-            ReplyToAddresses=[os.getenv("SES_EMAIL")],
-            Content={
-                "Simple": {
-                    "Subject": {
-                        "Data": "Error in sending email",
-                        "Charset": "UTF-8",
-                    },
-                    "Body": {
-                        "Html": {
-                            "Data": html_body,
-                            "Charset": "UTF-8",
-                        },
-                    },
-                },
-            },
-        )
-
-        result = "Error"
+    result: str = send_email.send_user_email(request_data)
 
     return wrap_response(
         body=json.dumps({"result": result}),
