82090a861ea5edd3dea305175edd60d7c69515cc
Eric Lundell <emlundell@alaska.edu>
2025-10-07 15:57:11 -0800

    Add custom log group

---
+++ portal-cdk/lambda_main/util/user_activity_logs_stream.py
@@ -0,0 +1,41 @@
+import json
+import os
+import time
+
+import boto3
+
+
+_logs = None
+
+
+def _get_logs_client() -> None:
+    global _logs
+    if not _logs:
+        _logs = boto3.client("logs", region_name=os.environ.get("region"))
+    return _logs
+
+
+def send_user_activity_logs(message: dict | str) -> dict:
+    """
+    message is either a dict or string of event inforoamtion to be sent to custom cloudwatch logs.
+    """
+    if isinstance(message, dict):
+        message = json.dumps(message)
+
+    logs_client = _get_logs_client()
+    log_group_name = os.environ.get("USER_ACTIVITY_LOGS_GROUP_NAME", None)
+    log_stream_name = os.environ.get("USER_ACTIVITY_LOGS_STREAM_NAME", None)
+
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/logs/client/put_log_events.html
+    response = logs_client.put_log_events(
+        logGroupName=log_group_name,
+        logStreamName=log_stream_name,
+        logEvents=[
+            {
+                "timestamp": int(time.time() * 1000),
+                "message": message,
+            }
+        ],
+    )
+
+    return response
