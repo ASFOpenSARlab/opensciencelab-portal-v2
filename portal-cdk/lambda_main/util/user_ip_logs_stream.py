65da5135d99c0ba5eb74e6655490da1f074fd884
Eric Lundell <emlundell@alaska.edu>
2025-10-08 13:42:38 -0800

    Fix to pass tests

--- portal-cdk/lambda_main/util/user_ip_logs_stream.py
+++ portal-cdk/lambda_main/util/user_ip_logs_stream.py
@@ -4,6 +4,9 @@ import time
 
 import boto3
 
+from aws_lambda_powertools import Logger
+
+logger = Logger(log_uncaught_exceptions=True)
 
 _logs = None
 
@@ -17,35 +20,33 @@ def _get_logs_client() -> None:
 
 def send_user_ip_logs(message: dict | str) -> dict:
     """
-    message is either a dict or string of event inforoamtion to be sent to custom cloudwatch logs.
+    `message` is either a dict or string of event information to be sent to custom cloudwatch logs.
     """
     if isinstance(message, dict):
         message = json.dumps(message)
+
+    event = {
+        "timestamp": int(time.time() * 1000),
+        "message": message,
+    }
 
     logs_client = _get_logs_client()
     log_group_name = os.environ.get("USER_IP_LOGS_GROUP_NAME", None)
     log_stream_name = os.environ.get("USER_IP_LOGS_STREAM_NAME", None)
 
-    if not log_group_name:
-        raise Exception(
-            "User Activity Log Group Name not defined. Did you set the environment variable?"
+    if not log_group_name or not log_stream_name:
+        logger.warning(
+            "User Activity Log Group or Stream not defined. Did you set the environment variable?"
         )
-
-    if not log_stream_name:
-        raise Exception(
-            "User Activity Log Stream Name not defined. Did you set the environment variable?"
-        )
+        logger.warning("User IP event not collected in special log group. Event: ")
+        logger.warning(json.dumps(event))
+        return {}
 
     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/logs/client/put_log_events.html
     response = logs_client.put_log_events(
         logGroupName=log_group_name,
         logStreamName=log_stream_name,
-        logEvents=[
-            {
-                "timestamp": int(time.time() * 1000),
-                "message": message,
-            }
-        ],
+        logEvents=[event],
     )
 
     return response
