ba01c504aab05d06c57cba11ebd91f7460e59770
Eric Lundell <emlundell@alaska.edu>
2025-10-09 11:17:46 -0800

    Only capture human endpoint traffic

--- portal-cdk/lambda_main/util/auth.py
+++ portal-cdk/lambda_main/util/auth.py
@@ -356,22 +356,26 @@ def require_access(access="user", human: bool = False):
                     cookies=cookies,
                 )
 
-            ip_address_with_port = current_session.app.current_event.get(
-                "headers", {}
-            ).get("cloudfront-viewer-address", "0.0.0.0")
-            country_code = current_session.app.current_event.get("headers", {}).get(
-                "cloudfront-viewer-country", "ZZ"
-            )
+            # Makes sure that capture IPs for human endpoints only.
+            # Access role is included in log so that we can filter admin traffic
+            if human:
+                ip_address_with_port = current_session.app.current_event.get(
+                    "headers", {}
+                ).get("cloudfront-viewer-address", "0.0.0.0")
+                country_code = current_session.app.current_event.get("headers", {}).get(
+                    "cloudfront-viewer-country", "ZZ"
+                )
 
-            ip_address = ip_address_with_port.split(":")[0]
+                ip_address = ip_address_with_port.split(":")[0]
 
-            send_user_ip_logs(
-                {
-                    "ip_address": ip_address,
-                    "country_code": country_code,
-                    "username": username,
-                }
-            )
+                send_user_ip_logs(
+                    {
+                        "ip_address": ip_address,
+                        "country_code": country_code,
+                        "username": username,
+                        "access_roles": ",".join(current_session.user.access),
+                    }
+                )
 
             # Check if user is disabled:
             if current_session.user.is_locked:
