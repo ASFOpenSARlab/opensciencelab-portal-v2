d0cc14161a04f9681f4b24b518a9eabb0e540921
Eric Lundell <emlundell@alaska.edu>
2025-10-09 16:03:24 -0800

    Get user ip logs function

--- portal-cdk/lambda_main/util/user_ip_logs_stream.py
+++ portal-cdk/lambda_main/util/user_ip_logs_stream.py
@@ -1,6 +1,7 @@
 import json
 import os
 import time
+import datetime
 
 import boto3
 
@@ -51,3 +52,46 @@ def send_user_ip_logs(message: dict | str) -> dict:
     )
 
     return response
+
+
+def get_user_ip_logs(
+    query: str, start_date: datetime.datetime = None, end_date: datetime.datetime = None
+) -> dict:
+    if not end_date:
+        end_date = datetime.datetime.now(datetime.timezone.utc)
+    end_date_int = int(end_date.timestamp())
+
+    if not start_date:
+        start_date = end_date - datetime.timedelta(days=30)
+    start_date_int = int(start_date.timestamp())
+
+    logs_client = _get_logs_client()
+
+    log_group_name = os.environ.get("USER_IP_LOGS_GROUP_NAME", None)
+    log_stream_name = os.environ.get("USER_IP_LOGS_STREAM_NAME", None)
+
+    if not log_group_name or not log_stream_name:
+        logger.warning(
+            "User Activity Log Group or Stream not defined. Did you set the environment variable?"
+        )
+        return {}
+
+    start_query_response = logs_client.start_query(
+        logGroupName=log_group_name,
+        startTime=start_date_int,
+        endTime=end_date_int,
+        queryString=query,
+    )
+
+    query_id = start_query_response["queryId"]
+
+    response = None
+
+    time_to_wait = 0
+
+    while response == None or response["status"] == "Running":
+        print("Waiting for query to complete ...")
+        time.sleep(time_to_wait)
+        response = logs_client.get_query_results(queryId=query_id)
+
+    return response["results"]
