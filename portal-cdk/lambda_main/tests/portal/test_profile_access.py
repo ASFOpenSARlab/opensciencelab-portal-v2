b1288bdd29624391de3f731e3997642a02095ee5
Cameron Showalter <cjshowalter@alaska.edu>
2025-10-15 15:34:11 -0800

    Broke out test_profile into it's two classes, so each one have their own file now

---
+++ portal-cdk/lambda_main/tests/portal/test_profile_access.py
@@ -0,0 +1,193 @@
+import os
+
+
+import boto3
+from moto import mock_aws
+
+import main
+
+REGION = os.getenv("STACK_REGION", "us-west-2")
+USER_IP_LOGS_GROUP_NAME = "FAKE_USER_IP_LOGS_GROUP_NAME"
+USER_IP_LOGS_STREAM_NAME = "FAKE_USER_IP_LOGS_STREAM_NAME"
+
+
+@mock_aws
+class TestProfileAccess:
+
+    def setup_method(self, method):
+        # Logs need to be created since the profiles use @require_access which populates the log group
+        self.logs_client = boto3.client("logs", region_name=REGION)
+
+        self.logs_client.create_log_group(
+            logGroupName=USER_IP_LOGS_GROUP_NAME
+        )
+
+        self.logs_client.create_log_stream(
+            logGroupName=USER_IP_LOGS_GROUP_NAME, logStreamName=USER_IP_LOGS_STREAM_NAME
+        )
+    # Test user accessing other profile
+    def test_user_access_other_profile(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        user = helpers.FakeUser()
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
+
+        path = "/portal/profile/form/other_user"
+        event = helpers.get_event(path=path, cookies=fake_auth)
+        ret = main.lambda_handler(event, lambda_context)
+
+        assert ret["statusCode"] == 302
+        assert ret["body"] == "\"{'Redirect to /portal/profile/form/test_user'}\""
+        assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
+
+    # Test user accessing own profile
+    def test_user_access_own_profile(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        user = helpers.FakeUser()
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
+
+        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
+        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
+        monkeypatch.setattr("util.user_ip_logs_stream._logs_client", self.logs_client)
+
+        path = "/portal/profile/form/test_user"
+        event = helpers.get_event(path=path, cookies=fake_auth)
+        ret = main.lambda_handler(event, lambda_context)
+
+        assert ret["statusCode"] == 200
+        assert ret["body"].find("Hello <i>test_user</i>") != -1
+        assert ret["headers"].get("Content-Type") == "text/html"
+
+    # Test user redirect for required profile filling
+    def test_user_profile_redirect_flag(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        user = helpers.FakeUser(require_profile_update=True)
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
+
+        path = "/portal"
+        event = helpers.get_event(path=path, cookies=fake_auth)
+        ret = main.lambda_handler(event, lambda_context)
+
+        assert ret["statusCode"] == 302
+        assert ret["body"] == "User must update profile"
+        assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
+
+    # Test user accessing other profile while required to fill own profile
+    def test_user_access_other_profile_must_fill_own(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        user = helpers.FakeUser(require_profile_update=True)
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
+
+        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
+        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
+        monkeypatch.setattr("util.user_ip_logs_stream._logs_client", self.logs_client)
+
+        path = "/portal/profile/form/test_user"
+        event = helpers.get_event(path=path, cookies=fake_auth)
+        ret = main.lambda_handler(event, lambda_context)
+
+        assert ret["statusCode"] == 200
+        assert ret["body"].find("Hello <i>test_user</i>") != -1
+        assert ret["headers"].get("Content-Type") == "text/html"
+
+    # Test admin accessing other profile
+    def test_admin_access_other_profile(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        admin_user = helpers.FakeUser(username="admin_user", access=["admin", "user"])
+        profile_user = helpers.FakeUser(username="other_user", access=["user"])
+
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: admin_user)
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: profile_user)
+
+        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
+        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
+        monkeypatch.setattr("util.user_ip_logs_stream._logs_client", self.logs_client)
+
+        path = "/portal/profile/form/other_user"
+        event = helpers.get_event(path=path, cookies=fake_auth)
+        ret = main.lambda_handler(event, lambda_context)
+
+        assert ret["statusCode"] == 200
+        assert ret["body"].find("Hello <i>other_user</i>") != -1
+        assert ret["headers"].get("Content-Type") == "text/html"
+        # Admin should see settings for other_user:
+        assert ret["body"].find("Admin Settings") > -1
+
+    # Test admin redirect for required profile filling
+    def test_admin_profile_redirect_flag(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        user = helpers.FakeUser(require_profile_update=True, access=["admin", "user"])
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
+
+        path = "/portal"
+        event = helpers.get_event(path=path, cookies=fake_auth)
+        ret = main.lambda_handler(event, lambda_context)
+
+        assert ret["statusCode"] == 302
+        assert ret["body"] == "User must update profile"
+        assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
+
+    # Test admin accessing other profile while required to fill own profile
+    def test_admin_access_other_profile_must_fill_own(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        user = helpers.FakeUser(require_profile_update=True, access=["admin", "user"])
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
+
+        path = "/portal/profile/form/other_user"
+        event = helpers.get_event(path=path, cookies=fake_auth)
+        ret = main.lambda_handler(event, lambda_context)
+
+        assert ret["statusCode"] == 302
+        assert ret["body"] == "User must update profile"
+        assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
+
+    def test_normal_user_no_table(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        user = helpers.FakeUser()
+
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
+
+        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
+        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
+        monkeypatch.setattr("util.user_ip_logs_stream._logs_client", self.logs_client)
+
+        event = helpers.get_event(
+            path="/portal/profile/form/test_user", cookies=fake_auth
+        )
+        ret = main.lambda_handler(event, lambda_context)
+        assert ret["statusCode"] == 200
+        assert not ret["headers"].get("Location")
+        assert ret["body"].find("Admin Settings") == -1
+
+    def test_admin_user_with_table(
+        self, monkeypatch, lambda_context, fake_auth, helpers
+    ):
+        user = helpers.FakeUser(access=["user", "admin"])
+
+        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
+        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
+
+        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
+        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
+        monkeypatch.setattr("util.user_ip_logs_stream._logs_client", self.logs_client)
+
+        event = helpers.get_event(
+            path="/portal/profile/form/test_user", cookies=fake_auth
+        )
+        ret = main.lambda_handler(event, lambda_context)
+        assert ret["statusCode"] == 200
+        assert ret["body"].find("Admin Settings") > -1
--- portal-cdk/lambda_main/tests/portal/test_profile_pages.py
+++ portal-cdk/lambda_main/tests/portal/test_profile_pages.py
@@ -21,13 +21,13 @@ class TestProfilePages:
         import util
 
         # Logs need to be created since the profiles use @require_access which populates the log group
-        util.user_ip_logs_stream._logs_client = boto3.client("logs", region_name=REGION)
+        self.logs_client = boto3.client("logs", region_name=REGION)
 
-        util.user_ip_logs_stream._logs_client.create_log_group(
+        self.logs_client.create_log_group(
             logGroupName=USER_IP_LOGS_GROUP_NAME
         )
 
-        util.user_ip_logs_stream._logs_client.create_log_stream(
+        self.logs_client.create_log_stream(
             logGroupName=USER_IP_LOGS_GROUP_NAME, logStreamName=USER_IP_LOGS_STREAM_NAME
         )
 
@@ -54,6 +54,7 @@ class TestProfilePages:
 
         monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
         monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
+        monkeypatch.setattr("util.user_ip_logs_stream._logs_client", self.logs_client)
 
         event = helpers.get_event(
             path="/portal/profile/form/test_user", cookies=fake_auth
@@ -121,6 +122,7 @@ class TestProfilePages:
 
         monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
         monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
+        monkeypatch.setattr("util.user_ip_logs_stream._logs_client", self.logs_client)
 
         qparams = {
             "country_of_residence_error": "missing",
@@ -182,6 +184,7 @@ class TestProfilePages:
 
         monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
         monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
+        monkeypatch.setattr("util.user_ip_logs_stream._logs_client", self.logs_client)
 
         path = "/portal/profile/form/test_user"
         event = helpers.get_event(path=path, cookies=fake_auth)
@@ -517,174 +520,3 @@ class TestProfilePages:
         assert ret["statusCode"] == 302
         assert ret["body"] == "User must update profile"
         assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
-
-
-class TestProfileAccess:
-    def setup_class():
-        ## These imports have to be the long forum, to let us modify the values here:
-        # https://stackoverflow.com/a/12496239/11650472
-        import util
-
-        util.user_ip_logs_stream._logs_client = boto3.client("logs", region_name=REGION)
-
-    # Test user accessing other profile
-    def test_user_access_other_profile(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        user = helpers.FakeUser()
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
-
-        path = "/portal/profile/form/other_user"
-        event = helpers.get_event(path=path, cookies=fake_auth)
-        ret = main.lambda_handler(event, lambda_context)
-
-        assert ret["statusCode"] == 302
-        assert ret["body"] == "\"{'Redirect to /portal/profile/form/test_user'}\""
-        assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
-
-    # Test user accessing own profile
-    def test_user_access_own_profile(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        user = helpers.FakeUser()
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
-
-        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
-        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
-
-        path = "/portal/profile/form/test_user"
-        event = helpers.get_event(path=path, cookies=fake_auth)
-        ret = main.lambda_handler(event, lambda_context)
-
-        assert ret["statusCode"] == 200
-        assert ret["body"].find("Hello <i>test_user</i>") != -1
-        assert ret["headers"].get("Content-Type") == "text/html"
-
-    # Test user redirect for required profile filling
-    def test_user_profile_redirect_flag(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        user = helpers.FakeUser(require_profile_update=True)
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
-
-        path = "/portal"
-        event = helpers.get_event(path=path, cookies=fake_auth)
-        ret = main.lambda_handler(event, lambda_context)
-
-        assert ret["statusCode"] == 302
-        assert ret["body"] == "User must update profile"
-        assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
-
-    # Test user accessing other profile while required to fill own profile
-    def test_user_access_other_profile_must_fill_own(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        user = helpers.FakeUser(require_profile_update=True)
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
-
-        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
-        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
-
-        path = "/portal/profile/form/test_user"
-        event = helpers.get_event(path=path, cookies=fake_auth)
-        ret = main.lambda_handler(event, lambda_context)
-
-        assert ret["statusCode"] == 200
-        assert ret["body"].find("Hello <i>test_user</i>") != -1
-        assert ret["headers"].get("Content-Type") == "text/html"
-
-    # Test admin accessing other profile
-    def test_admin_access_other_profile(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        admin_user = helpers.FakeUser(username="admin_user", access=["admin", "user"])
-        profile_user = helpers.FakeUser(username="other_user", access=["user"])
-
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: admin_user)
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: profile_user)
-
-        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
-        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
-
-        path = "/portal/profile/form/other_user"
-        event = helpers.get_event(path=path, cookies=fake_auth)
-        ret = main.lambda_handler(event, lambda_context)
-
-        assert ret["statusCode"] == 200
-        assert ret["body"].find("Hello <i>other_user</i>") != -1
-        assert ret["headers"].get("Content-Type") == "text/html"
-        # Admin should see settings for other_user:
-        assert ret["body"].find("Admin Settings") > -1
-
-    # Test admin redirect for required profile filling
-    def test_admin_profile_redirect_flag(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        user = helpers.FakeUser(require_profile_update=True, access=["admin", "user"])
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
-
-        path = "/portal"
-        event = helpers.get_event(path=path, cookies=fake_auth)
-        ret = main.lambda_handler(event, lambda_context)
-
-        assert ret["statusCode"] == 302
-        assert ret["body"] == "User must update profile"
-        assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
-
-    # Test admin accessing other profile while required to fill own profile
-    def test_admin_access_other_profile_must_fill_own(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        user = helpers.FakeUser(require_profile_update=True, access=["admin", "user"])
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
-
-        path = "/portal/profile/form/other_user"
-        event = helpers.get_event(path=path, cookies=fake_auth)
-        ret = main.lambda_handler(event, lambda_context)
-
-        assert ret["statusCode"] == 302
-        assert ret["body"] == "User must update profile"
-        assert ret["headers"].get("Location") == "/portal/profile/form/test_user"
-
-    def test_normal_user_no_table(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        user = helpers.FakeUser()
-
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
-
-        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
-        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
-
-        event = helpers.get_event(
-            path="/portal/profile/form/test_user", cookies=fake_auth
-        )
-        ret = main.lambda_handler(event, lambda_context)
-        assert ret["statusCode"] == 200
-        assert not ret["headers"].get("Location")
-        assert ret["body"].find("Admin Settings") == -1
-
-    def test_admin_user_with_table(
-        self, monkeypatch, lambda_context, fake_auth, helpers
-    ):
-        user = helpers.FakeUser(access=["user", "admin"])
-
-        monkeypatch.setattr("portal.profile.User", lambda *args, **kwargs: user)
-        monkeypatch.setattr("util.auth.User", lambda *args, **kwargs: user)
-
-        monkeypatch.setenv("USER_IP_LOGS_GROUP_NAME", USER_IP_LOGS_GROUP_NAME)
-        monkeypatch.setenv("USER_IP_LOGS_STREAM_NAME", USER_IP_LOGS_STREAM_NAME)
-
-        event = helpers.get_event(
-            path="/portal/profile/form/test_user", cookies=fake_auth
-        )
-        ret = main.lambda_handler(event, lambda_context)
-        assert ret["statusCode"] == 200
-        assert ret["body"].find("Admin Settings") > -1
