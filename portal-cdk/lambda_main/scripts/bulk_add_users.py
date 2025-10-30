1e1135ae8674bf1597991ec851cf9b465ae2805a
segallagher <segallagher@alaska.edu>
2025-10-30 11:44:05 -0800

    fix: simplify default value propegation, add arg validation and give example CLI calling

--- portal-cdk/lambda_main/scripts/bulk_add_users.py
+++ portal-cdk/lambda_main/scripts/bulk_add_users.py
@@ -1,8 +1,13 @@
 import argparse
 import requests
 
-## Adds users to a lab in bulk
-## Can be used in the command line or by defining the following variables below
+# Commandline use example
+# python bulk_add_users.py --portal-jwt <jwt cookie> --portal-username <username cookie> \
+# --lab-shortname smce-test-opensarlab --domain opensciencelab-test.asf.alaska.edu \
+# --users user1 user2 user3 --profiles "SAR 1,t3a.medium - Dask User,m6a.large"
+
+# You can specify the defaults for any/all of these parameters as below
+
 ## EXAMPLE
 # PORTAL_JWT = "<JWT Cookie>"
 # PORTAL_USERNAME = "<Username Cookie>"
@@ -10,15 +15,16 @@ import requests
 # DOMAIN = "opensciencelab-test.asf.alaska.edu"
 # USERS = [f"user{num}" for num in range(1,10)]
 # PROFILES = "m6a.large,m6a.xlarge"
-PORTAL_JWT = None
-PORTAL_USERNAME = None
-LAB_SHORTNAME = "smce-test-opensarlab"
-DOMAIN = "d1xhhnfz3jf3f5.cloudfront.net"
-USERS = [f"user{num}" for num in range(10,20)]
-PROFILES = "m6a.large,m6a.xlarge"
+PORTAL_JWT:str = None
+PORTAL_USERNAME:str = None
+LAB_SHORTNAME:str = None
+DOMAIN:str = None
+USERS:list[str] = None
+PROFILES:str = None
 
 
 def main():
+    # Add commandline arguments
     parser = argparse.ArgumentParser(
         description="Bulk adds users to a lab with a set of default profiles"
     )
@@ -27,25 +33,28 @@ def main():
         "--portal-jwt",
         type=str,
         help="active jwt cookie for portal",
+        default=PORTAL_JWT,
     )
 
     parser.add_argument(
         "--portal-username",
         type=str,
         help="active username cookie for portal",
+        default=PORTAL_USERNAME,
     )
 
     parser.add_argument(
         "--lab-shortname",
         type=str,
         help="shortname of lab which users will be added to",
+        default=LAB_SHORTNAME,
     )
 
     parser.add_argument(
         "--domain",
         type=str,
         help="domain of lab deployment",
-        # default="opensciencelab-test.asf.alaska.edu",
+        default=DOMAIN,
     )
 
     parser.add_argument(
@@ -53,35 +62,42 @@ def main():
         type=str,
         nargs='+',
         help="list of users to be added",
+        default=USERS,
     )
 
     parser.add_argument(
         "--profiles",
         type=str,
         help="profiles to be given to each user, comma separated string",
+        default=PROFILES,
     )
 
+    # Parse and validate args
     args = parser.parse_args()
+    missing_vals = []
+    for arg, val in vars(args).items():
+        if not val:
+            missing_vals.append(arg)
+    if len(missing_vals) > 0:
+        raise ValueError(f"Values not provided: {', '.join(missing_vals)}")
 
-    domain = DOMAIN if DOMAIN else args.domain
-    lab_shortname = LAB_SHORTNAME if LAB_SHORTNAME else args.lab_shortname
-    url = f"https://{domain}/portal/access/manage/{lab_shortname}/edituser"
+    # Generate endpoint url
+    url = f"https://{args.domain}/portal/access/manage/{args.lab_shortname}/edituser"
 
     # Cookies, move to argparser when complete
     cookies = {
-        "portal-jwt": PORTAL_JWT if PORTAL_JWT else args.portal_jwt,
-        "portal-username": PORTAL_USERNAME if PORTAL_USERNAME else args.portal_username,
+        "portal-jwt": args.portal_jwt,
+        "portal-username": args.portal_username,
     }
 
-    users = USERS if USERS else args.users
-    for username in users:
+    for username in args.users:
         # Generate user creation data
         data = {
             "username": username,
             "action": "add_user",
         }
         data.update({
-            "lab_profiles": PROFILES,
+            "lab_profiles": args.profiles,
             "time_quota": "",
             "lab_country_status": "something",
         })
@@ -93,7 +109,9 @@ def main():
             cookies=cookies,
         )
 
-        if ret.status_code != 200:
+        if ret.status_code == 200:
+            print(f"Added {username} to {args.lab_shortname} on {args.domain}")
+        else:
             print(f"Failed to create user \"{username}\"")
 
 
