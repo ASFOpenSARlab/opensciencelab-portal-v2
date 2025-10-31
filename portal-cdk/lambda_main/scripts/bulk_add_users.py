8ee48aa1cf50298d9ea45b18705fd28222a9e6ee
segallagher <segallagher@alaska.edu>
2025-10-30 16:13:05 -0800

    feat: custom validation, add --delete functionality, users defined in file only now

--- portal-cdk/lambda_main/scripts/bulk_add_users.py
+++ portal-cdk/lambda_main/scripts/bulk_add_users.py
@@ -2,25 +2,54 @@ import argparse
 import requests
 
 # Commandline use example
-# python bulk_add_users.py --portal-jwt <jwt cookie> --portal-username <username cookie> \
-# --lab-shortname smce-test-opensarlab --domain opensciencelab-test.asf.alaska.edu \
-# --users user1 user2 user3 --profiles "SAR 1,t3a.medium - Dask User,m6a.large"
+# usage: bulk_add_users.py [-h] [--portal-jwt PORTAL_JWT] [--portal-username PORTAL_USERNAME]
+#                          [--lab-shortname LAB_SHORTNAME] [--domain DOMAIN] [--users USERS [USERS ...]]
+#                          [--profiles PROFILES]
 
-# You can specify the defaults for any/all of these parameters as below
+# Bulk adds users to a lab with a set of default profiles
+# options:
+#   -h, --help            show this help message and exit
+#   --portal-jwt PORTAL_JWT
+#                         active jwt cookie for portal
+#   --portal-username PORTAL_USERNAME
+#                         active username cookie for portal
+#   --lab-shortname LAB_SHORTNAME
+#                         shortname of lab which users will be added to
+#   --domain DOMAIN       domain of lab deployment
+#   --users USERS [USERS ...]
+#                         list of users to be added
+#   --profiles PROFILES   profiles to be given to each user, comma separated string
 
-## EXAMPLE
-# PORTAL_JWT = "<JWT Cookie>"
-# PORTAL_USERNAME = "<Username Cookie>"
-# LAB_SHORTNAME = "smce-test-opensarlab"
-# DOMAIN = "opensciencelab-test.asf.alaska.edu"
-# USERS = [f"user{num}" for num in range(1,10)]
-# PROFILES = "m6a.large,m6a.xlarge"
-PORTAL_JWT: str = None
-PORTAL_USERNAME: str = None
-LAB_SHORTNAME: str = None
-DOMAIN: str = None
-USERS: list[str] = None
-PROFILES: str = None
+def read_user_file(path:str) -> list[str]:
+    with open(path, 'r') as f:
+        users = []
+        for line in f.readlines():
+            # Add user if line is not empty
+            if line.strip():
+                users.append(line.strip())
+        return users
+
+def validate_arguments(args) -> list[str]:
+    # validate required for deleting users
+    if args.delete and not args.users_file:
+        raise argparse.ArgumentTypeError("Required arguments when --delete is set: --users_file")
+
+    # validate required for adding users
+    missing_args = []
+    if not args.delete and not args.portal_jwt:
+        missing_args.append("--portal-jwt")
+    if not args.delete and not args.portal_username:
+        missing_args.append("--portal-username")
+    if not args.delete and not args.lab_shortname:
+        missing_args.append("--lab-shortname")
+    if not args.delete and not args.domain:
+        missing_args.append("--domain")
+    if not args.delete and not args.users_file:
+        missing_args.append("--users")
+    if not args.delete and not args.profiles:
+        missing_args.append("--profiles")
+    if missing_args:
+        raise argparse.ArgumentTypeError(f"Required arguments when --delete is NOT set: {", ".join(missing_args)}")
 
 
 def main():
@@ -33,53 +62,47 @@ def main():
         "--portal-jwt",
         type=str,
         help="active jwt cookie for portal",
-        default=PORTAL_JWT,
     )
 
     parser.add_argument(
         "--portal-username",
         type=str,
         help="active username cookie for portal",
-        default=PORTAL_USERNAME,
     )
 
     parser.add_argument(
         "--lab-shortname",
         type=str,
         help="shortname of lab which users will be added to",
-        default=LAB_SHORTNAME,
     )
 
     parser.add_argument(
         "--domain",
         type=str,
         help="domain of lab deployment",
-        default=DOMAIN,
     )
 
     parser.add_argument(
-        "--users",
+        "--users_file",
         type=str,
-        nargs="+",
-        help="list of users to be added",
-        default=USERS,
+        help="file where list of users to be modified is defined, file is of usernames separated by newlines",
     )
 
     parser.add_argument(
         "--profiles",
         type=str,
         help="profiles to be given to each user, comma separated string",
-        default=PROFILES,
+    )
+
+    parser.add_argument(
+        "--delete",
+        action="store_true",
+        help="if provided will delete the provided users",
     )
 
     # Parse and validate args
     args = parser.parse_args()
-    missing_vals = []
-    for arg, val in vars(args).items():
-        if not val:
-            missing_vals.append(arg)
-    if len(missing_vals) > 0:
-        raise ValueError(f"Values not provided: {', '.join(missing_vals)}")
+    validate_arguments(args)
 
     # Generate endpoint url
     url = f"https://{args.domain}/portal/access/manage/{args.lab_shortname}/edituser"
@@ -90,19 +113,16 @@ def main():
         "portal-username": args.portal_username,
     }
 
-    for username in args.users:
+    users = read_user_file(args.users_file)
+    for username in users:
         # Generate user creation data
         data = {
             "username": username,
             "action": "add_user",
+            "lab_profiles": args.profiles,
+            "time_quota": "",
+            "lab_country_status": "something",
         }
-        data.update(
-            {
-                "lab_profiles": args.profiles,
-                "time_quota": "",
-                "lab_country_status": "something",
-            }
-        )
 
         # Add user
         ret = requests.post(
@@ -114,7 +134,7 @@ def main():
         if ret.status_code == 200:
             print(f"Added {username} to {args.lab_shortname} on {args.domain}")
         else:
-            print(f'Failed to create user "{username}"')
+            raise Exception(f'Failed to create user "{username}"\nResponse body: {ret.text}')
 
 
 if __name__ == "__main__":
