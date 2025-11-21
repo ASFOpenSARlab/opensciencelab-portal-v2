d4e1f1c78c88940bb5694ca69dce88116ad72ecf
Eric Lundell <emlundell@alaska.edu>
2025-11-21 14:44:56 -0900

    Add refined Access query and fix some user profile edge cases

--- utilities/dump_users.py
+++ utilities/dump_users.py
@@ -33,18 +33,18 @@ SELECT
    t1.last_activity as last_update,
 
    -- user profile information
-   p1.country_of_residence as "profile.country_of_residence",
-   p1.faculty_member_affliated_with_university as "profile.faculty_member_affliated_with_university",
-   p1.graduate_student_affliated_with_university as "profile.graduate_student_affliated_with_university",
-   p1.is_affliated_with_nasa_research as "profile.is_affiliated_with_nasa",
-   p1.is_affliated_with_gov_research as "profile.is_affiliated_with_us_gov_research",
-   p1.is_affliated_with_isro_research as "profile.is_affliated_with_isro_research",
-   p1.is_affliated_with_university as "profile.is_affliated_with_university",
-   p1.pi_affliated_with_nasa_research_email as "profile.pi_affliated_with_nasa_research_email",
-   p1.research_member_affliated_with_university as "profile.research_member_affliated_with_university",
-   p1.user_affliated_with_gov_research_email as "profile.user_affliated_with_gov_research_email",
-   p1.user_affliated_with_isro_research_email as "profile.user_affliated_with_isro_research_email",
-   p1.user_affliated_with_nasa_research_email as "profile.user_affliated_with_nasa_research_email",
+   COAL(p1.country_of_residence) as "profile.country_of_residence",
+   COAL(p1.faculty_member_affliated_with_university) as "profile.faculty_member_affliated_with_university",
+   COAL(p1.graduate_student_affliated_with_university) as "profile.graduate_student_affliated_with_university",
+   COAL(p1.is_affliated_with_nasa_research) as "profile.is_affiliated_with_nasa",
+   COAL(p1.is_affliated_with_gov_research) as "profile.is_affiliated_with_us_gov_research",
+   COAL(p1.is_affliated_with_isro_research) as "profile.is_affliated_with_isro_research",
+   COAL(p1.is_affliated_with_university) as "profile.is_affliated_with_university",
+   COAL(p1.pi_affliated_with_nasa_research_email) as "profile.pi_affliated_with_nasa_research_email",
+   COAL(p1.research_member_affliated_with_university) as "profile.research_member_affliated_with_university",
+   COAL(p1.user_affliated_with_gov_research_email) as "profile.user_affliated_with_gov_research_email",
+   COAL(p1.user_affliated_with_isro_research_email) as "profile.user_affliated_with_isro_research_email",
+   COAL(p1.user_affliated_with_nasa_research_email) as "profile.user_affliated_with_nasa_research_email",
    --p1.user_or_pi_nasa_email as "profile.user_or_pi_nasa_email",
 
    p1.force_update as require_profile_update,
@@ -79,20 +79,124 @@ WHERE
 ORDER by t1.last_activity ASC;
 """
 
-ACCESS_SQL = """
-select 
-   lab_short_name,
-   username,
-   lab_profiles 
-from 
-   useretc.access
-where
-   username = ?;
+ACCESS_SQL = r"""
+WITH 
+	wildcard AS (
+		/*
+		 * Wildcard * "default" usernames need to be applied to all usernames in the lab.
+		 * If there is a wildcard present, get the lab and profiles.
+		 */
+		SELECT
+			lab_short_name,
+			lab_profiles
+		FROM access
+		WHERE username = '*'
+		GROUP BY lab_short_name, username
+	),
+	consolidated AS (
+		SELECT 
+			access.lab_short_name, 
+			access.username, 
+			access.lab_profiles
+		FROM access
+		WHERE 
+			/*
+			 * IF the special !! is found anywhere in the access sheet for the lab, the whole lab is disregarded.
+			 */
+			lab_short_name NOT IN (
+				SELECT a.lab_short_name
+				FROM access AS a 
+				WHERE a.username LIKE '!!%'
+			)
+			/* 
+			 * USERNAME that starts with - or _ are used as either placeholders or something not to be used.
+			 * The Access sheet also has blanks '' that we don't need.
+			 */
+			AND (
+				username NOT LIKE '\_%' ESCAPE '\'
+				AND username NOT LIKE '-%'
+				AND username NOT LIKE ''
+			)
+			/*
+			 * Wildcard "Default" * usernames are meaningless in Portal v2
+			 */
+			AND (
+				username <> "*" 
+		    	AND username <> "!*"
+			)
+		GROUP BY lab_short_name, username
+		HAVING
+			/*
+		 	* The not ! username means to block the user from the lab. So disregard all instances of the username in the lab. 
+		 	*/
+			username NOT LIKE '!%'
+		ORDER BY lab_short_name, username
+	)
+SELECT
+	consolidated.lab_short_name AS lab_short_name,
+	consolidated.username AS username,
+	/*
+	* Select all concated lab profiles, the wildcard default AND also grouped by lab and username, trimmed of whitespace and extra commmas, replacing NULL with " ".
+	* A registered Python function "REMOVE_NEAGTED" is used to remove negated (!) profiles. 
+	*/
+	REMOVE_NEGATED(
+        trim(
+            trim(
+                group_concat(
+                    coalesce(consolidated.lab_profiles, " ") || "," || coalesce(wildcard.lab_profiles, " "),
+                    ","
+                )
+            ),
+        ",")
+    ) AS lab_profiles
+FROM consolidated
+	/*
+	 * Since all rows in a lab need the defaults, left join only by the lab name.
+	 */
+	LEFT JOIN wildcard
+		ON wildcard.lab_short_name = consolidated.lab_short_name
+WHERE consolidated.username == ?
+GROUP BY consolidated.lab_short_name, consolidated.username
 """
+
+
+# Define custom sql function
+def remove_negated(profiles: str) -> str:
+    # Split profiles into a list
+    list_profiles: list = profiles.split(",")
+
+    # Get all profiles that start with !
+    exclaim_profiles = set(str(u) for u in list_profiles if str(u).startswith("!"))
+
+    # Get all profiles that would be negated by it's conjugate
+    unexclaim_profiles = set(u.lstrip("!") for u in exclaim_profiles)
+
+    # Get the full list of profiles to be deleted
+    profiles_to_delete = list(exclaim_profiles | unexclaim_profiles)
+
+    # Find all profiles in the original list that are not removed
+    usable_profiles = [p for p in list_profiles if p not in profiles_to_delete]
+
+    # Return comma seperated
+    return ",".join(usable_profiles)
+
+
+# Make sure some Python values behave nicely with SQL
+# Name taken after the SQL function COALESCE
+def coal(user_profile_value: str) -> str:
+    if user_profile_value is None:
+        user_profile_value = "none"
+
+    return user_profile_value
+
 
 # Connect to DBS
 db = sqlite3.connect(f"file:{JUPYTER_HUB_DB}?mode=ro", uri=True)
 db.row_factory = sqlite3.Row  # Enable column fetching
+
+db.create_function("REMOVE_NEGATED", 1, remove_negated)
+db.create_function("COAL", 1, coal)
+
 cur = db.cursor()
 
 # Join other DB's
@@ -148,72 +252,80 @@ for row in rows:
         "profile": {
             "M": {
                 "country_of_residence": {
-                    "S": export_object["profile.country_of_residence"]
+                    "S": export_object.get("profile.country_of_residence", "")
                 },
                 "faculty_member_affliated_with_university": {
-                    "BOOL": export_object[
-                        "profile.faculty_member_affliated_with_university"
-                    ]
+                    "BOOL": export_object.get(
+                        "profile.faculty_member_affliated_with_university", "No"
+                    )
                     == "Yes"
                 },
                 "graduate_student_affliated_with_university": {
-                    "BOOL": export_object[
-                        "profile.graduate_student_affliated_with_university"
-                    ]
+                    "BOOL": export_object.get(
+                        "profile.graduate_student_affliated_with_university", "No"
+                    )
                     == "Yes"
                 },
                 "is_affiliated_with_nasa": {
-                    "S": export_object["profile.is_affiliated_with_nasa"].lower()
+                    "S": export_object.get(
+                        "profile.is_affiliated_with_nasa", ""
+                    ).lower()
+                    or None
                 },
                 "is_affiliated_with_us_gov_research": {
-                    "S": export_object[
-                        "profile.is_affiliated_with_us_gov_research"
-                    ].lower()
+                    "S": export_object.get(
+                        "profile.is_affiliated_with_us_gov_research", ""
+                    ).lower()
                 },
                 "is_affliated_with_isro_research": {
-                    "S": export_object[
-                        "profile.is_affliated_with_isro_research"
-                    ].lower()
+                    "S": export_object.get(
+                        "profile.is_affliated_with_isro_research", ""
+                    ).lower()
                 },
                 "is_affliated_with_university": {
-                    "S": export_object["profile.is_affliated_with_university"].lower()
+                    "S": export_object.get(
+                        "profile.is_affliated_with_university", ""
+                    ).lower()
                 },
                 "pi_affliated_with_nasa_research_email": {
-                    "S": export_object[
-                        "profile.pi_affliated_with_nasa_research_email"
-                    ].lower()
+                    "S": export_object.get(
+                        "profile.pi_affliated_with_nasa_research_email", ""
+                    ).lower()
                 },
                 "research_member_affliated_with_university": {
-                    "BOOL": export_object[
-                        "profile.research_member_affliated_with_university"
-                    ]
+                    "BOOL": export_object.get(
+                        "profile.research_member_affliated_with_university", "No"
+                    )
                     == "Yes"
                 },
                 "user_affliated_with_gov_research_email": {
-                    "S": export_object[
-                        "profile.user_affliated_with_gov_research_email"
-                    ].lower()
+                    "S": export_object.get(
+                        "profile.user_affliated_with_gov_research_email", ""
+                    ).lower()
                 },
                 "user_affliated_with_isro_research_email": {
-                    "S": export_object[
-                        "profile.user_affliated_with_isro_research_email"
-                    ].lower()
+                    "S": export_object.get(
+                        "profile.user_affliated_with_isro_research_email", ""
+                    ).lower()
                 },
                 "user_affliated_with_nasa_research_email": {
-                    "S": export_object[
-                        "profile.user_affliated_with_nasa_research_email"
-                    ].lower()
+                    "S": export_object.get(
+                        "profile.user_affliated_with_nasa_research_email", ""
+                    ).lower()
                 },
             }
         },
         "random_dict": {"NULL": True},
         "require_profile_update": {
-            "BOOL": True if export_object["require_profile_update"] == 1 else False
+            "BOOL": True
+            if export_object.get("require_profile_update", 1) == 1
+            else False
         },
         "some_int_without_default": {"NULL": True},
         "_rec_counter": {"N": export_object["_rec_counter"]},
     }
 
+    print("----")
     print(json.dumps(export_object, indent=2))
     print(json.dumps(crafted_export, indent=2))
 
