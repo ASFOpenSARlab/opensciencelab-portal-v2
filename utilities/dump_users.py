f11f5991221ee159292e86a824aafb49f70ecc18
Dylan Palmieri <dgpalmieri@gmail.com>
2025-11-24 17:27:22 -0900

    feat: add users to dynamo properly optionally add users to cognito with `--cognito`

--- utilities/dump_users.py
+++ utilities/dump_users.py
@@ -1,5 +1,60 @@
 import json
 import sqlite3
+import os
+import boto3
+
+_DYNAMO_CLIENT, _DYNAMO_DB, _DYNAMO_TABLE = None, None, None
+
+# Cognito Client
+cog_client = boto3.client("cognito-idp", region_name="us-west-2")
+
+# Get cmd line args
+parser = argparse.ArgumentParser(description="Migrate users from OSL Portal v1 to v2")
+parser.add_argument(
+    "-c",
+    "--cognito",
+    dest="cognito",
+    action="store_true",
+    help="Create cognito accounts for all users",
+)
+args = parser.parse_args()
+
+# ---------------------- FILL THESE OUT BEFORE RUNNING ----------------------
+MIGRATION_DYNAMO_TABLE = ""
+MIGRATION_COGNITO_USER_POOL = ""
+
+
+def _get_dynamo():
+    """
+    Lazy load all DynamoDB stuff since it takes forever the first time.
+    """
+    global _DYNAMO_CLIENT, _DYNAMO_DB, _DYNAMO_TABLE  # pylint: disable=global-statement
+    region = os.getenv("STACK_REGION", "us-west-2")
+    if not _DYNAMO_CLIENT:
+        _DYNAMO_CLIENT = boto3.client("dynamodb", region_name=region)
+    if not _DYNAMO_DB:
+        _DYNAMO_DB = boto3.resource("dynamodb", region_name=region)
+    if not _DYNAMO_TABLE:
+        _DYNAMO_TABLE = _DYNAMO_DB.Table(MIGRATION_DYNAMO_TABLE)
+    return _DYNAMO_CLIENT, _DYNAMO_DB, _DYNAMO_TABLE
+
+
+def create_item(item: dict) -> bool:
+    """
+    Creates an item in the DB.
+    """
+    _client, _db, table = _get_dynamo()
+    # "Cast" to a plain dict, so it can be serialized to JSON.
+    item = json.loads(json.dumps(item, default=str))
+    RESTRICTED_KEYS = []
+    for restricted_key in RESTRICTED_KEYS:
+        if restricted_key in item:
+            raise ValueError(
+                f"Can't set '{restricted_key}', that's one we set automatically and WILL get overridden."
+            )
+    table.put_item(Item=item)
+    return True
+
 
 USER_ETC_DB = "/home/ec2-user/code/services/srv/useretc/db/useretc.db"
 JUPYTER_HUB_DB = "/home/ec2-user/code/services/srv/portal/jupyterhub/jupyterhub.sqlite"
@@ -81,65 +136,65 @@ ORDER by t1.last_activity ASC;
 
 ACCESS_SQL = r"""
 WITH 
-	wildcard AS (
-		/*
-		 * Wildcard * "default" usernames need to be applied to all usernames in the lab.
-		 * If there is a wildcard present, get the lab and profiles.
-		 */
-		SELECT
-			lab_short_name,
-			lab_profiles
-		FROM access
-		WHERE username = '*'
-		GROUP BY lab_short_name, username
-	),
-	consolidated AS (
-		SELECT 
-			access.lab_short_name, 
-			access.username, 
-			access.lab_profiles
-		FROM access
-		WHERE 
-			/*
-			 * IF the special !! is found anywhere in the access sheet for the lab, the whole lab is disregarded.
-			 */
-			lab_short_name NOT IN (
-				SELECT a.lab_short_name
-				FROM access AS a 
-				WHERE a.username LIKE '!!%'
-			)
-			/* 
-			 * USERNAME that starts with - or _ are used as either placeholders or something not to be used.
-			 * The Access sheet also has blanks '' that we don't need.
-			 */
-			AND (
-				username NOT LIKE '\_%' ESCAPE '\'
-				AND username NOT LIKE '-%'
-				AND username NOT LIKE ''
-			)
-			/*
-			 * Wildcard "Default" * usernames are meaningless in Portal v2
-			 */
-			AND (
-				username <> "*" 
-		    	AND username <> "!*"
-			)
-		GROUP BY lab_short_name, username
-		HAVING
-			/*
-		 	* The not ! username means to block the user from the lab. So disregard all instances of the username in the lab. 
-		 	*/
-			username NOT LIKE '!%'
-		ORDER BY lab_short_name, username
-	)
+        wildcard AS (
+                /*
+                 * Wildcard * "default" usernames need to be applied to all usernames in the lab.
+                 * If there is a wildcard present, get the lab and profiles.
+                 */
+                SELECT
+                        lab_short_name,
+                        lab_profiles
+                FROM access
+                WHERE username = '*'
+                GROUP BY lab_short_name, username
+        ),
+        consolidated AS (
+                SELECT 
+                        access.lab_short_name, 
+                        access.username, 
+                        access.lab_profiles
+                FROM access
+                WHERE 
+                        /*
+                         * IF the special !! is found anywhere in the access sheet for the lab, the whole lab is disregarded.
+                         */
+                        lab_short_name NOT IN (
+                                SELECT a.lab_short_name
+                                FROM access AS a 
+                                WHERE a.username LIKE '!!%'
+                        )
+                        /* 
+                         * USERNAME that starts with - or _ are used as either placeholders or something not to be used.
+                         * The Access sheet also has blanks '' that we don't need.
+                         */
+                        AND (
+                                username NOT LIKE '\_%' ESCAPE '\'
+                                AND username NOT LIKE '-%'
+                                AND username NOT LIKE ''
+                        )
+                        /*
+                         * Wildcard "Default" * usernames are meaningless in Portal v2
+                         */
+                        AND (
+                                username <> "*" 
+                        AND username <> "!*"
+                        )
+                GROUP BY lab_short_name, username
+                HAVING
+                        /*
+                        * The not ! username means to block the user from the lab. So disregard all instances of the username in the lab. 
+                        */
+                        username NOT LIKE '!%'
+                ORDER BY lab_short_name, username
+        )
 SELECT
-	consolidated.lab_short_name AS lab_short_name,
-	consolidated.username AS username,
-	/*
-	* Select all concated lab profiles, the wildcard default AND also grouped by lab and username, trimmed of whitespace and extra commmas, replacing NULL with " ".
-	* A registered Python function "REMOVE_NEAGTED" is used to remove negated (!) profiles. 
-	*/
-	REMOVE_NEGATED(
+        consolidated.lab_short_name AS lab_short_name,
+        consolidated.username AS username,
+        /*
+        * Select all concated lab profiles, the wildcard default AND also grouped by lab and username, trimmed of whitespace and extra commmas, replacing NULL with " ".
+        * A registered Python function "REMOVE_NEAGTED" is used to remove negated (!) profiles. 
+        */
+        REMOVE_NEGATED(
         trim(
             trim(
                 group_concat(
@@ -150,11 +205,11 @@ SELECT
         ",")
     ) AS lab_profiles
 FROM consolidated
-	/*
-	 * Since all rows in a lab need the defaults, left join only by the lab name.
-	 */
-	LEFT JOIN wildcard
-		ON wildcard.lab_short_name = consolidated.lab_short_name
+        /*
+         * Since all rows in a lab need the defaults, left join only by the lab name.
+         */
+        LEFT JOIN wildcard
+                ON wildcard.lab_short_name = consolidated.lab_short_name
 WHERE consolidated.username == ?
 GROUP BY consolidated.lab_short_name, consolidated.username
 """
@@ -185,7 +240,7 @@ def remove_negated(profiles: str) -> str:
 # Name taken after the SQL function COALESCE
 def coal(user_profile_value: str) -> str:
     if user_profile_value is None:
-        user_profile_value = "none"
+        user_profile_value = ""
 
     return user_profile_value
 
@@ -222,111 +277,82 @@ for row in rows:
     export_object["labs"] = labs
 
     crafted_export = {
-        "username": {"S": export_object["username"]},
-        "access": {"L": [{"S": "user"}, {"S": "admin"}]}
-        if export_object["is_admin"] == 1
-        else {"L": [{"S": "user"}]},
-        "country_code": {"S": export_object["country_code"]},
-        "created_at": {"S": export_object["created_at"]},
-        "email": {"S": export_object["email"]},
-        "ip_address": {"S": export_object["ip_address"]},
-        "is_locked": {"BOOL": export_object["is_locked"] == 1},
+        "username": export_object["username"],
+        "access": ["user", "admin"] if export_object["is_admin"] == 1 else ["user"],
+        "country_code": export_object["country_code"],
+        "created_at": export_object["created_at"],
+        "email": export_object["email"],
+        "ip_address": export_object["ip_address"],
+        "is_locked": export_object["is_locked"] == 1,
         "labs": {
-            "M": {
-                entry["lab_short_name"]: {
-                    "M": {
-                        "lab_profiles": {
-                            "L": []
-                            if entry["lab_profiles"] is None
-                            or entry["lab_profiles"] == ""
-                            else [n.strip() for n in entry["lab_profiles"].split(",")]
-                        }
-                    }
-                }
+            entry["lab_short_name"]: {
+                "lab_profiles": []
+                if entry["lab_profiles"] is None or entry["lab_profiles"] == ""
+                else [n.strip() for n in entry["lab_profiles"].split(",")]
             }
             for entry in export_object["labs"]
             if entry["lab_short_name"] in active_labs
         },
-        "last_cookie_assignment": {"S": export_object["last_cookie_assignment"]},
-        "last_update": {"S": export_object["last_update"]},
+        "last_cookie_assignment": export_object["last_cookie_assignment"],
+        "last_update": export_object["last_update"],
         "profile": {
-            "M": {
-                "country_of_residence": {
-                    "S": export_object.get("profile.country_of_residence", "")
-                },
-                "faculty_member_affliated_with_university": {
-                    "BOOL": export_object.get(
-                        "profile.faculty_member_affliated_with_university", "No"
-                    )
-                    == "Yes"
-                },
-                "graduate_student_affliated_with_university": {
-                    "BOOL": export_object.get(
-                        "profile.graduate_student_affliated_with_university", "No"
-                    )
-                    == "Yes"
-                },
-                "is_affiliated_with_nasa": {
-                    "S": export_object.get(
-                        "profile.is_affiliated_with_nasa", ""
-                    ).lower()
-                    or None
-                },
-                "is_affiliated_with_us_gov_research": {
-                    "S": export_object.get(
-                        "profile.is_affiliated_with_us_gov_research", ""
-                    ).lower()
-                },
-                "is_affliated_with_isro_research": {
-                    "S": export_object.get(
-                        "profile.is_affliated_with_isro_research", ""
-                    ).lower()
-                },
-                "is_affliated_with_university": {
-                    "S": export_object.get(
-                        "profile.is_affliated_with_university", ""
-                    ).lower()
-                },
-                "pi_affliated_with_nasa_research_email": {
-                    "S": export_object.get(
-                        "profile.pi_affliated_with_nasa_research_email", ""
-                    ).lower()
-                },
-                "research_member_affliated_with_university": {
-                    "BOOL": export_object.get(
-                        "profile.research_member_affliated_with_university", "No"
-                    )
-                    == "Yes"
-                },
-                "user_affliated_with_gov_research_email": {
-                    "S": export_object.get(
-                        "profile.user_affliated_with_gov_research_email", ""
-                    ).lower()
-                },
-                "user_affliated_with_isro_research_email": {
-                    "S": export_object.get(
-                        "profile.user_affliated_with_isro_research_email", ""
-                    ).lower()
-                },
-                "user_affliated_with_nasa_research_email": {
-                    "S": export_object.get(
-                        "profile.user_affliated_with_nasa_research_email", ""
-                    ).lower()
-                },
-            }
+            "country_of_residence": export_object.get(
+                "profile.country_of_residence", ""
+            ),
+            "faculty_member_affliated_with_university": export_object.get(
+                "profile.faculty_member_affliated_with_university", "No"
+            )
+            == "Yes",
+            "graduate_student_affliated_with_university": export_object.get(
+                "profile.graduate_student_affliated_with_university", "No"
+            )
+            == "Yes",
+            "is_affiliated_with_nasa": export_object.get(
+                "profile.is_affiliated_with_nasa", ""
+            ).lower()
+            or None,
+            "is_affiliated_with_us_gov_research": export_object.get(
+                "profile.is_affiliated_with_us_gov_research", ""
+            ).lower(),
+            "is_affliated_with_isro_research": export_object.get(
+                "profile.is_affliated_with_isro_research", ""
+            ).lower(),
+            "is_affliated_with_university": export_object.get(
+                "profile.is_affliated_with_university", ""
+            ).lower(),
+            "pi_affliated_with_nasa_research_email": export_object.get(
+                "profile.pi_affliated_with_nasa_research_email", ""
+            ).lower(),
+            "research_member_affliated_with_university": export_object.get(
+                "profile.research_member_affliated_with_university", "No"
+            )
+            == "Yes",
+            "user_affliated_with_gov_research_email": export_object.get(
+                "profile.user_affliated_with_gov_research_email", ""
+            ).lower(),
+            "user_affliated_with_isro_research_email": export_object.get(
+                "profile.user_affliated_with_isro_research_email", ""
+            ).lower(),
+            "user_affliated_with_nasa_research_email": export_object.get(
+                "profile.user_affliated_with_nasa_research_email", ""
+            ).lower(),
+            "user_or_pi_nasa_email": "none",
         },
-        "random_dict": {"NULL": True},
-        "require_profile_update": {
-            "BOOL": True
-            if export_object.get("require_profile_update", 1) == 1
-            else False
-        },
-        "some_int_without_default": {"NULL": True},
-        "_rec_counter": {"N": export_object["_rec_counter"]},
+        "require_profile_update": True
+        if export_object.get("require_profile_update", 1) == 1
+        else False,
+        "_rec_counter": export_object["_rec_counter"],
     }
 
     print("----")
     print(json.dumps(export_object, indent=2))
     print(json.dumps(crafted_export, indent=2))
 
-    break
+    _ = create_item(crafted_export)
+
+    if args.cognito:
+        cog_client.admin_create_user(
+            UserPoolId=MIGRATION_COGNITO_USER_POOL,
+            Username=crafted_export["username"],
+            UserAttributes=[{"Name": "email", "Value": crafted_export["email"]}],
+        )
