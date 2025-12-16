f77ec0a40f17578748198981e7b6b26401378bd8
segallagher <segallagher@alaska.edu>
2025-12-16 12:16:23 -0900

    fix: better profile default and page auto fill

--- portal-cdk/lambda_main/util/labs/base_lab.py
+++ portal-cdk/lambda_main/util/labs/base_lab.py
@@ -23,7 +23,7 @@ class BaseLab:
         }
     )
     crypto_remediation_role_arn: str = None
-    default_profiles: list = None
+    default_profiles: list = []
 
 
 daac_limited_restricted_status = {
