f73804334d35d48a27899651c282cd153af9a0a1
Brian Buechler <btbuechler@alaska.edu>
2025-12-19 15:43:27 -0700

    Disable certificate validation for health checks
    Prod api-gateway lab deployments are advertising an `asf.alaska.edu` SSL cert

--- portal-cdk/lambda_main/util/labs/base_lab.py
+++ portal-cdk/lambda_main/util/labs/base_lab.py
@@ -31,6 +31,7 @@ class BaseLab:
             ret = requests.get(
                 url=f"{self.deployment_url}/lab/{self.short_lab_name}/hub/health",
                 timeout=0.1,
+                verify=False,
             )
         except requests.exceptions.ReadTimeout:
             return False
